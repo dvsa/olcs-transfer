111535c69e9141e0bb4eb2086e0490e4759a36ca
Rob Caiger <rob@clocal.co.uk>
2015-05-07 16:24:36 +0100

    Module skeleton

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dvsa\Olcs\Transfer;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module implements AutoloaderProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/Transfer',
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
---
+++ config/module.config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'router' => [
+        'routes' => [
+
+        ]
+    ]
+];
