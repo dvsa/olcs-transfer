cee4fe662b9b28881725e4cf78c44d456a710779
Rob Caiger <rob@clocal.co.uk>
2015-06-01 17:23:07 +0100

    Array Validation annotations

---
+++ src/Util/Annotation/ArrayValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ArrayValidator
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace Dvsa\Olcs\Transfer\Util\Annotation;
+
+/**
+ * @Annotation
+ */
+class ArrayValidator extends Validator
+{
+}
