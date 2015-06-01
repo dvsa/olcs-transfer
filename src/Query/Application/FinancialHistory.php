e3211ddb7f704289f0cbfacbe13d4c8e54ecfe1a
Chris Riley <chris.riley@valtech.co.uk>
2015-06-01 10:28:14 +0100

    Removing inheritance from Query DTO

--- src/Query/Application/FinancialHistory.php
+++ src/Query/Application/FinancialHistory.php
@@ -13,7 +13,17 @@ use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 /**
  * @Transfer\RouteName("backend/application/single/financial-history")
  */
-class FinancialHistory extends Application
+class FinancialHistory extends AbstractQuery
 {
-    //
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
 }
