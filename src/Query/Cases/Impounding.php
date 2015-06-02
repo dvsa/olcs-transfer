92b9f426aa92070fb97e433b5a252dca3808cdf6
Shaun Lizzio <shaun@lizzio.co.uk>
2015-06-02 13:20:51 +0100

    Added Impounding / list Query objects. working

--- src/Query/Cases/Impounding.php
+++ src/Query/Cases/Impounding.php
@@ -4,19 +4,20 @@ namespace Dvsa\Olcs\Transfer\Query\Cases;
 
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
-use Dvsa\Olcs\Transfer\Query\OrderedQueryInterface;
-use Dvsa\Olcs\Transfer\Query\PagedQueryInterface;
-use Dvsa\Olcs\Transfer\Query\PagedTrait;
-use Dvsa\Olcs\Transfer\Query\OrderedTrait;
 
 /**
- * Class ImpoundingList
- * @Transfer\RouteName("backend/impoundings")
+ * Class Impounding
+ * @Transfer\RouteName("backend/impounding/single")
  */
-class ImpoundingList extends AbstractQuery implements PagedQueryInterface, OrderedQueryInterface
+class Impounding extends AbstractQuery
 {
-    use PagedTrait;
-    use OrderedTrait;
+    /**
+     * @var int
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $id;
 
     /**
      * @var int
@@ -25,6 +26,14 @@ class ImpoundingList extends AbstractQuery implements PagedQueryInterface, Order
      * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
      */
     protected $case;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
     /**
      * @return int
