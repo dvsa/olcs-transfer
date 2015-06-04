dd9f3dfc5874ddd5c41ebad89ee38858efe1f779
Shaun Lizzio <shaun@lizzio.co.uk>
2015-06-04 15:03:01 +0100

    Added Delete Command

--- src/Command/Cases/Complaint/DeleteComplaint.php
+++ src/Command/Cases/Complaint/DeleteComplaint.php
@@ -1,7 +1,59 @@
 <?php
+
+namespace Dvsa\Olcs\Transfer\Command\Cases\Complaint;
+
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+
 /**
- * Created by PhpStorm.
- * User: slizzio
- * Date: 04/06/15
- * Time: 14:47
- */ 
+ * @Transfer\RouteName("backend/complaint")
+ * @Transfer\Method("DELETE")
+ */
+class DeleteComplaint extends AbstractCommand
+{
+    /**
+     * @todo add validators
+     */
+    protected $id = null;
+
+    /**
+     * @todo add validators
+     */
+    protected $version = null;
+
+    /**
+     * @param mixed $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $version
+     * @return $this
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+}
