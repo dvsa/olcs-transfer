f8431902c74a28ff8225b34bc787ccdebb524ad3
Shaun Lizzio <shaun@lizzio.co.uk>
2015-06-09 09:31:55 +0100

    Added Get/ List query objects

---
+++ src/Query/Cases/EnvironmentalComplaint/EnvironmentalComplaint.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Dvsa\Olcs\Transfer\Query\Cases\EnvironmentalComplaint;
+
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+
+/**
+ * Class Compaint
+ * @Transfer\RouteName("backend/environmental-complaint/single")
+ */
+class EnvironmentalComplaint extends AbstractQuery
+{
+    /**
+     * @var int
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $id;
+
+    /**
+     * @var int
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $case;
+
+    /**
+     * @Transfer\Optional()
+     * isCompliance = 1 unless Environmental
+     */
+    protected $isCompliance = 0;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCase()
+    {
+        return $this->case;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsCompliance()
+    {
+        return $this->isCompliance;
+    }
+}
---
+++ src/Query/Cases/EnvironmentalComplaint/EnvironmentalComplaintList.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Dvsa\Olcs\Transfer\Query\Cases\EnvironmentalComplaint;
+
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Query\OrderedQueryInterface;
+use Dvsa\Olcs\Transfer\Query\PagedQueryInterface;
+use Dvsa\Olcs\Transfer\Query\PagedTrait;
+use Dvsa\Olcs\Transfer\Query\OrderedTrait;
+
+/**
+ * Class EnvironmentalComplaintList
+ * @Transfer\RouteName("backend/environmental-complaint")
+ */
+class EnvironmentalComplaintList extends AbstractQuery implements PagedQueryInterface, OrderedQueryInterface
+{
+    use PagedTrait;
+    use OrderedTrait;
+
+    /**
+     * @var int
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $case;
+
+    /**
+     * @Transfer\Optional()
+     * isCompliance = true unless Environmental
+     */
+    protected $isCompliance = 0;
+
+    /**
+     * @return int
+     */
+    public function getCase()
+    {
+        return $this->case;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsCompliance()
+    {
+        return $this->isCompliance;
+    }
+}
