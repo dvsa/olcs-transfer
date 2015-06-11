42a688a7bb75fb249b7fb82f57c25d140af28f2e
Nick Payne <nick@kurai.co.uk>
2015-06-11 12:17:41 +0100

    create abstract command inherited by concrete LVAs for updating addresses

--- src/Command/Licence/UpdateAddresses.php
+++ src/Command/Licence/UpdateAddresses.php
@@ -1,115 +1,19 @@
 <?php
 
 /**
- * Licence Addresses
+ * Licence Update Addresses
  *
  * @author Nick Payne <nick.payne@valtech.co.uk>
- *
- * @FIXME: validators required!
  */
 namespace Dvsa\Olcs\Transfer\Command\Licence;
 
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
-use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+use Dvsa\Olcs\Transfer\Command\AbstractUpdateAddresses;
 
 /**
  * @Transfer\RouteName("backend/licence/single/addresses")
  * @Transfer\Method("PUT")
  */
-final class UpdateAddresses extends AbstractCommand
+final class UpdateAddresses extends AbstractUpdateAddresses
 {
-    /**
-     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
-     */
-    protected $id;
-
-    /**
-     * @Transfer\Optional
-     */
-    protected $correspondence;
-
-    /**
-     * @Transfer\Optional
-     */
-    protected $contact;
-
-    /**
-     * @Transfer\Partial("Dvsa\Olcs\Transfer\Command\Partial\Address")
-     * @Transfer\Optional
-     */
-    protected $correspondenceAddress;
-
-    /**
-     * @Transfer\Optional
-     */
-    protected $establishment;
-
-    /**
-     * @Transfer\Partial("Dvsa\Olcs\Transfer\Command\Partial\Address")
-     * @Transfer\Optional
-     */
-    protected $establishmentAddress;
-
-    /**
-     * @Transfer\Optional
-     */
-    protected $consultant;
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCorrespondence()
-    {
-        return $this->correspondence;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getContact()
-    {
-        return $this->contact;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCorrespondenceAddress()
-    {
-        return $this->correspondenceAddress;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEstablishment()
-    {
-        return $this->establishment;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEstablishmentAddress()
-    {
-        return $this->establishmentAddress;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getConsultant()
-    {
-        return $this->consultant;
-    }
 }
