818d70364f7bd52f64fe2c05dd4a8dee3fc2a37d
Piotr Filak <piotr.filak@valtech.co.uk>
2015-06-12 16:02:31 +0100

    OLCS-9436 - Internal - Admin - Recipient BE

---
+++ src/Command/Publication/CreateRecipient.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * Create Recipient
+ */
+namespace Dvsa\Olcs\Transfer\Command\Publication;
+
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+
+/**
+ * @Transfer\RouteName("backend/publication/recipient")
+ * @Transfer\Method("POST")
+ */
+final class CreateRecipient extends AbstractCommand
+{
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"Y", "N"}}})
+     */
+    protected $isObjector;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options": {"max":100}})
+     */
+    protected $contactName;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\EmailAddress"})
+     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options": {"max":90}})
+     */
+    protected $emailAddress;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"Y", "N"}}})
+     */
+    protected $sendAppDecision;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"Y", "N"}}})
+     */
+    protected $sendNoticesProcs;
+
+    /**
+     * @Transfer\ArrayInput
+     * @Transfer\ArrayFilter({"name":"Dvsa\Olcs\Transfer\Filter\FilterEmptyItems"})
+     * @Transfer\ArrayFilter({"name":"Dvsa\Olcs\Transfer\Filter\UniqueItems"})
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options":{"min": 1}})
+     */
+    protected $trafficAreas;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"Y", "N"}}})
+     * @Transfer\Optional
+     */
+    protected $isPolice;
+
+    /**
+     * @return mixed
+     */
+    public function getIsObjector()
+    {
+        return $this->isObjector;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContactName()
+    {
+        return $this->contactName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmailAddress()
+    {
+        return $this->emailAddress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSendAppDecision()
+    {
+        return $this->sendAppDecision;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSendNoticesProcs()
+    {
+        return $this->sendNoticesProcs;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTrafficAreas()
+    {
+        return $this->trafficAreas;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsPolice()
+    {
+        return $this->isPolice;
+    }
+}
---
+++ src/Command/Publication/UpdateRecipient.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * Update Recipient
+ */
+namespace Dvsa\Olcs\Transfer\Command\Publication;
+
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+
+/**
+ * @Transfer\RouteName("backend/publication/recipient/single")
+ * @Transfer\Method("PUT")
+ */
+final class UpdateRecipient extends AbstractCommand
+{
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $id;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $version;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"Y", "N"}}})
+     */
+    protected $isObjector;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options": {"max":100}})
+     */
+    protected $contactName;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\EmailAddress"})
+     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options": {"max":90}})
+     */
+    protected $emailAddress;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"Y", "N"}}})
+     */
+    protected $sendAppDecision;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"Y", "N"}}})
+     */
+    protected $sendNoticesProcs;
+
+    /**
+     * @Transfer\ArrayInput
+     * @Transfer\ArrayFilter({"name":"Dvsa\Olcs\Transfer\Filter\FilterEmptyItems"})
+     * @Transfer\ArrayFilter({"name":"Dvsa\Olcs\Transfer\Filter\UniqueItems"})
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options":{"min": 1}})
+     */
+    protected $trafficAreas;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"Y", "N"}}})
+     * @Transfer\Optional
+     */
+    protected $isPolice;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsObjector()
+    {
+        return $this->isObjector;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContactName()
+    {
+        return $this->contactName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmailAddress()
+    {
+        return $this->emailAddress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSendAppDecision()
+    {
+        return $this->sendAppDecision;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSendNoticesProcs()
+    {
+        return $this->sendNoticesProcs;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTrafficAreas()
+    {
+        return $this->trafficAreas;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsPolice()
+    {
+        return $this->isPolice;
+    }
+}
