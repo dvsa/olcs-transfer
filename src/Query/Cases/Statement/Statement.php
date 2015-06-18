3a211956daa61fb3879bd87068d5035220207626
Shaun Lizzio <shaun@lizzio.co.uk>
2015-06-18 15:01:56 +0100

    Added routes and query handlers

---
+++ src/Query/Cases/Statement/Statement.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Dvsa\Olcs\Transfer\Query\Cases\Statement;
+
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+
+/**
+ * Class Statement
+ * @Transfer\RouteName("backend/statement/single")
+ */
+class Statement extends AbstractQuery
+{
+    /**
+     * @var int
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $id;
+
+    /**
+     * @var int
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $case;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCase()
+    {
+        return $this->case;
+    }
+}
