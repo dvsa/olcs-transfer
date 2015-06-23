6fd7b917e44f7efec6f92ad57cc254055e0824a3
Chris Riley <chris.riley@valtech.co.uk>
2015-06-23 10:51:44 +0100

    Turned route config into a glob of many configs

---
+++ config/backend-route.template.php
@@ -0,0 +1,13 @@
+<?php
+
+use Dvsa\Olcs\Transfer\Command;
+use Dvsa\Olcs\Transfer\Query;
+use Dvsa\Olcs\Transfer\Router\CommandConfig;
+use Dvsa\Olcs\Transfer\Router\QueryConfig;
+use Dvsa\Olcs\Transfer\Router\RouteConfig;
+
+return [
+    'route-name' => [
+        //route information
+    ]
+];
