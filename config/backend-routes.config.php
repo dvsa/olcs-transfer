a5d251362103c0ff7e7cb99f1d18b805263cb8ff
Mat Evans <mat.evans@valtech.co.uk>
2015-06-23 17:44:28 +0100

    OLCS-10018 DTO get list of TMA's

--- config/backend-routes.config.php
+++ config/backend-routes.config.php
@@ -1119,49 +1119,6 @@ $routes = [
                     'POST' => CommandConfig::getPostConfig(Command\Document\CreateDocument::class),
                 ]
             ],
-            'transport-manager-application' => [
-                'type' => 'Segment',
-                'options' => [
-                    'route' => 'transport-manager-application[/]',
-                    'defaults' => [
-                        'id' => null
-                    ]
-                ],
-                'may_terminate' => false,
-                'child_routes' => [
-                    'single' => RouteConfig::getSingleConfig(
-                        [
-                            'GET' => QueryConfig::getConfig(Query\TransportManagerApplication\GetDetails::class),
-                            'update-status' => [
-                                'type' => 'Segment',
-                                'options' => [
-                                    'route' => 'update-status[/]',
-                                ],
-                                'may_terminate' => false,
-                                'child_routes' => [
-                                    'PUT' => CommandConfig::getPutConfig(
-                                        Command\TransportManagerApplication\UpdateStatus::class
-                                    ),
-                                ]
-                            ],
-                            'update-details' => [
-                                'type' => 'Segment',
-                                'options' => [
-                                    'route' => 'update-details[/]',
-                                ],
-                                'may_terminate' => false,
-                                'child_routes' => [
-                                    'PUT' => CommandConfig::getPutConfig(
-                                        Command\TransportManagerApplication\UpdateDetails::class
-                                    ),
-                                ]
-                            ],
-                        ]
-                    ),
-                    'POST' => CommandConfig::getPostConfig(Command\TransportManagerApplication\Create::class),
-                    'DELETE' => CommandConfig::getDeleteConfig(Command\TransportManagerApplication\Delete::class),
-                ]
-            ],
             'user' => [
                 'type' => 'Segment',
                 'options' => [
