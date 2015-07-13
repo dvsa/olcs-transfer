97c860b1c1008e92288d036c5ec505e254bbdd0e
Shaun Lizzio <shaun@lizzio.co.uk>
2015-07-13 14:10:53 +0100

    Added transfer routes for Commands and single query

---
+++ src/Command/Submission/CreateSubmissionSectionComment.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Create SubmissionSectionComment
+ */
+namespace Dvsa\Olcs\Transfer\Command\Submission;
+
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+
+/**
+ * @Transfer\RouteName("backend/submission-section-comment")
+ * @Transfer\Method("POST")
+ */
+final class CreateSubmissionSectionComment extends AbstractCommand
+{
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $submission;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({
+     *      "name":"Zend\Validator\InArray",
+     *      "options": {
+     *          "haystack": {
+     *              "introduction", "case-summary"
+     *          }
+     *      }
+     * })
+     */
+    protected $submissionSection;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     */
+    protected $comment;
+
+    /**
+     * @return mixed
+     */
+    public function getSubmission()
+    {
+        return $this->submission;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+}
