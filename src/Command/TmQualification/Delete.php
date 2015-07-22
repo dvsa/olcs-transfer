be1bac30502750a952183368830b368a1effbdb7
Chris Riley <chris.riley@valtech.co.uk>
2015-07-22 13:10:22 +0100

    Changed deleted tm qualification command to multi delete command

--- src/Command/TmQualification/Delete.php
+++ src/Command/TmQualification/Delete.php
@@ -2,8 +2,8 @@
 
 namespace Dvsa\Olcs\Transfer\Command\TmQualification;
 
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
-use Dvsa\Olcs\Transfer\Command\AbstractDeleteCommand;
 
 /**
  * Delete Inspection Request
@@ -11,7 +11,21 @@ use Dvsa\Olcs\Transfer\Command\AbstractDeleteCommand;
  * @Transfer\RouteName("backend/tm-qualification/single")
  * @Transfer\Method("DELETE")
  */
-class Delete extends AbstractDeleteCommand
+class Delete extends AbstractCommand
 {
-    //
+    /**
+     * @Transfer\ArrayInput
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $ids;
+
+    /**
+     * @return mixed
+     */
+    public function getIds()
+    {
+        return $this->ids;
+    }
 }
