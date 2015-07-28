edc23a8cb12968052ed474cac7cd93e0ef530670
Dan Eggleston <dan@stolenegg.com>
2015-07-28 17:27:11 +0100

    OLCS-3675 make address optional in contact details partial, make query cachable

--- src/Query/Operator/UnlicensedBusinessDetails.php
+++ src/Query/Operator/UnlicensedBusinessDetails.php
@@ -9,12 +9,13 @@ namespace Dvsa\Olcs\Transfer\Query\Operator;
 
 use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Query\CachableQueryInterface;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 
 /**
  * @Transfer\RouteName("backend/operator-unlicensed/single")
  */
-class UnlicensedBusinessDetails extends AbstractQuery
+class UnlicensedBusinessDetails extends AbstractQuery implements CachableQueryInterface
 {
     use Identity;
 }
