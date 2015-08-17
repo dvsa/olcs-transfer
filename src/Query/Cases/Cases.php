cbcffb0d214a4cd1dcf980bb2f37bc5b49cefba4
Piotr Filak <piotr.filak@valtech.co.uk>
2015-08-17 12:13:24 +0100

    OLCS-9932 - Listeners - Cases.php

--- src/Query/Cases/Cases.php
+++ src/Query/Cases/Cases.php
@@ -9,12 +9,13 @@ namespace Dvsa\Olcs\Transfer\Query\Cases;
 
 use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Query\CachableQueryInterface;
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 
 /**
  * @Transfer\RouteName("backend/cases/single")
  */
-class Cases extends AbstractQuery
+class Cases extends AbstractQuery implements CachableQueryInterface
 {
     use Identity;
 }
