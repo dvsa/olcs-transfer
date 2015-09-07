9754dc596d894e06bf19ab440d5be04a1f61c9d0
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-09-07 10:08:36 +0100

    Fix DTOs to just extend the abstract delete command

--- src/Command/Cases/Impounding/DeleteImpounding.php
+++ src/Command/Cases/Impounding/DeleteImpounding.php
@@ -2,64 +2,16 @@
 
 namespace Dvsa\Olcs\Transfer\Command\Cases\Impounding;
 
-use Dvsa\Olcs\Transfer\Command\AbstractCommand;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractDeleteCommand;
 
 /**
+ * Concrete delete class.
+ *
  * @Transfer\RouteName("backend/impounding/single")
  * @Transfer\Method("DELETE")
  */
-class DeleteImpounding extends AbstractCommand
+class DeleteImpounding extends AbstractDeleteCommand
 {
-    /**
-     * @var int
-     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
-     */
-    protected $id = null;
-
-    /**
-     * @var int
-     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
-     */
-    protected $version = null;
-
-    /**
-     * @param mixed $id
-     * @return $this
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $version
-     * @return $this
-     */
-    public function setVersion($version)
-    {
-        $this->version = $version;
-        return $this;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getVersion()
-    {
-        return $this->version;
-    }
+    //
 }
--- src/Command/EnvironmentalComplaint/DeleteEnvironmentalComplaint.php
+++ src/Command/EnvironmentalComplaint/DeleteEnvironmentalComplaint.php
@@ -2,59 +2,16 @@
 
 namespace Dvsa\Olcs\Transfer\Command\EnvironmentalComplaint;
 
-use Dvsa\Olcs\Transfer\Command\AbstractCommand;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractDeleteCommand;
 
 /**
+ * Concrete delete class.
+ *
  * @Transfer\RouteName("backend/environmental-complaint/single")
  * @Transfer\Method("DELETE")
  */
-class DeleteEnvironmentalComplaint extends AbstractCommand
+class DeleteEnvironmentalComplaint extends AbstractDeleteCommand
 {
-    /**
-     * @var int
-     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
-     */
-    protected $id = null;
-
-    /**
-     * @var int
-     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
-     */
-    protected $version = null;
-
-    /**
-     * @Transfer\Optional()
-     * @Transfer\Filter({"name":"Zend\Filter\Boolean"})
-     * @Transfer\Validator({"name":"Zend\Validator\Identical", "options": {"token": false}})
-     */
-    protected $isCompliance = false;
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getVersion()
-    {
-        return $this->version;
-    }
-
-    /**
-     * @return bool
-     */
-    public function getIsCompliance()
-    {
-        return $this->isCompliance;
-    }
+    //
 }
