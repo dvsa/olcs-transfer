622e5ed5001bbc122dca3ca34507aed3f9f21b79
Dan Eggleston <dan@stolenegg.com>
2015-09-10 13:09:02 +0100

    OLCS-9676 create dto for financial standing rate

---
+++ src/Command/System/CreateFinancialStandingRate.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Create Financial Standing Rate
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+namespace Dvsa\Olcs\Transfer\Command\System;
+
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+
+/**
+ * @Transfer\RouteName("backend/financial-standing-rate
+ * @Transfer\Method("POST")
+ */
+final class CreateFinancialStandingRate extends AbstractCommand
+{
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"lcat_gv","lcat_psv"}}})
+     * @Transfer\Optional
+     */
+    protected $goodsOrPsv;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"ltyp_r","ltyp_sn","ltyp_si","ltyp_sr"}}})
+     * @Transfer\Optional
+     */
+    protected $licenceType;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Dvsa\Olcs\Transfer\Validators\Money"})
+     */
+    protected $firstVehicleRate;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Dvsa\Olcs\Transfer\Validators\Money"})
+     */
+    protected $additionalVehicleRate;
+
+    /**
+     * @Transfer\Validator({"name": "Date", "options": {"format": "Y-m-d"}})
+     */
+    protected $effectiveFrom;
+
+    /**
+     * Gets the value of goodsOrPsv.
+     *
+     * @return mixed
+     */
+    public function getGoodsOrPsv()
+    {
+        return $this->goodsOrPsv;
+    }
+
+    /**
+     * Gets the value of licenceType.
+     *
+     * @return mixed
+     */
+    public function getLicenceType()
+    {
+        return $this->licenceType;
+    }
+
+    /**
+     * Gets the value of firstVehicleRate.
+     *
+     * @return mixed
+     */
+    public function getFirstVehicleRate()
+    {
+        return $this->firstVehicleRate;
+    }
+
+    /**
+     * Gets the value of additionalVehicleRate.
+     *
+     * @return mixed
+     */
+    public function getAdditionalVehicleRate()
+    {
+        return $this->additionalVehicleRate;
+    }
+
+    /**
+     * Gets the value of effectiveFrom.
+     *
+     * @return mixed
+     */
+    public function getEffectiveFrom()
+    {
+        return $this->effectiveFrom;
+    }
+}
