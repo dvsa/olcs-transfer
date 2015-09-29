04d08f3cec6b1b9c0b21adcae640ac5c9750fc20
Piotr Filak <piotr.filak@valtech.co.uk>
2015-09-29 15:52:02 +0100

    OLCS-10692 - Remove users fields

--- src/Command/User/CreateUser.php
+++ src/Command/User/CreateUser.php
@@ -80,13 +80,6 @@ final class CreateUser extends AbstractCommand
 
     /**
      * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
-     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options":{"max":10}})
-     * @Transfer\Optional
-     */
-    protected $memorableWord = null;
-
-    /**
-     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
      * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"Y", "N"}}})
      * @Transfer\Optional
      */
@@ -142,11 +135,6 @@ final class CreateUser extends AbstractCommand
     public function getLoginId()
     {
         return $this->loginId;
-    }
-
-    public function getMemorableWord()
-    {
-        return $this->memorableWord;
     }
 
     public function getMustResetPassword()
