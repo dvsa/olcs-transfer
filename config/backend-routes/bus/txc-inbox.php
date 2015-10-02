24a2468b9b11078f0b420d04c05a5991c22548e7
Shaun Lizzio <shaun@lizzio.co.uk>
2015-10-02 11:44:48 +0100

    Added child route to query txcinbox by bus reg

--- config/backend-routes/bus/txc-inbox.php
+++ config/backend-routes/bus/txc-inbox.php
@@ -7,11 +7,21 @@ use Dvsa\Olcs\Transfer\Router\QueryConfig;
 use Dvsa\Olcs\Transfer\Router\RouteConfig;
 
 return [
-    'txc-inbox' => RouteConfig::getRouteConfig(
-        'txc-inbox',
-        [
+    'txc-inbox' => [
+        'type' => 'Segment',
+        'options' => [
+            'route' => 'txc-inbox[/]',
+        ],
+        'may_terminate' => false,
+        'child_routes' => [
             'GET' => QueryConfig::getConfig(Query\Bus\Ebsr\TxcInboxList::class),
+            'bus-reg' => RouteConfig::getRouteConfig(
+                'bus-reg',
+                [
+                    'GET' => QueryConfig::getConfig(Query\Bus\Ebsr\TxcInboxByBusReg::class),
+                ]
+            ),
             'PUT' => CommandConfig::getPutConfig(Command\Bus\Ebsr\UpdateTxcInbox::class),
         ]
-    )
+    ]
 ];
