7eab8c4567a4ce6117f264fd90f2c200a13ce6a2
Shaun Lizzio <shaun@lizzio.co.uk>
2015-10-09 11:15:39 +0100

    Added Assign submission Command

---
+++ src/Command/Submission/AssignSubmission.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * AssignSubmission Submission
+ */
+namespace Dvsa\Olcs\Transfer\Command\Submission;
+
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+use Dvsa\Olcs\Transfer\FieldType as FieldType;
+
+/**
+ * @Transfer\RouteName("backend/submission/assign")
+ * @Transfer\Method("PUT")
+ */
+final class AssignSubmission extends AbstractCommand
+{
+    // Identity & Locking
+    use FieldType\Traits\Identity;
+    use FieldType\Traits\Version;
+
+    /**
+     * @Transfer\Optional
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $recipientUser;
+
+    /**
+     * @Transfer\Optional
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"Y", "N"}}})
+     */
+    protected $urgent;
+
+    /**
+     * @return mixed
+     */
+    public function getRecipientUser()
+    {
+        return $this->recipientUser;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrgent()
+    {
+        return $this->urgent;
+    }
+}
