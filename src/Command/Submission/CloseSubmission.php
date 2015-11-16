762aff4040b8ca97b758c213cc2c311d62aa889b
Shaun Lizzio <shaun@lizzio.co.uk>
2015-11-16 13:46:59 +0000

    Added DTOs for close and reopen

---
+++ src/Command/Submission/CloseSubmission.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dvsa\Olcs\Transfer\Command\Submission;
+
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractCloseCommand;
+
+/**
+ * Concrete close class.
+ *
+ * @Transfer\RouteName("backend/submission/single/close")
+ * @Transfer\Method("PUT")
+ */
+class CloseSubmission extends AbstractCloseCommand
+{
+    //
+}
