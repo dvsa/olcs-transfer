6682db69f59c21fe008da843550eef8d9d9af126
Dan Eggleston <dan@stolenegg.com>
2015-11-17 10:39:57 +0000

    OLCS-11260 update postcode validation to match CPMS logic

--- src/Validators/Postcode.php
+++ src/Validators/Postcode.php
@@ -16,47 +16,50 @@ use Zend\Validator\AbstractValidator;
  */
 class Postcode extends AbstractValidator
 {
-    /**
-     * Holds the templates
-     *
-     * @var array
-     */
-    protected $messageTemplates = array(
-        'invalid' => 'Invalid postcode'
-    );
+    const POSTCODE_MIN_LENGTH = 6;
+    const POSTCODE_MAX_LENGTH = 8;
+    const POSTCODE_BAD_LENGTH     = 'postcodeBadLength';
+    const POSTCODE_INVALID_FORMAT = 'invalidPostcodeFormat';
 
-    /**
-     * Holds the array of regexes
-     *
-     * (Regexes supplied by CPMS team via OLCS-11260)
-     */
-    protected $patterns = [
-       "/^[A-Z]{1,2}[0-9]{2,3}[A-Z]{2}$/",
-       "/^[A-Z]{1,2}[0-9]{1}[A-Z]{1}[0-9]{1}[A-Z]{2}$/",
-       "/^GIR0[A-Z]{2}$/",
+    protected $messageTemplates = [
+        self::POSTCODE_BAD_LENGTH     => 'Postcode must be either 6, 7 or 8 characters long and start with a letter',
+        self::POSTCODE_INVALID_FORMAT => 'Invalid postcode format'
+    ];
+
+    protected $postcodeFormats = [
+        '[A-Z]{1,2}[0-9]{2,3}[A-Z]{2}',
+        '[A-Z]{1,2}[0-9]{1}[A-Z]{1}[0-9]{1}[A-Z]{2}',
+        'GIR0[A-Z]{2}'
     ];
 
     /**
-     * Check if a postcode is valid
+     * Check if a postcode is valid. This re-implements the validation logic
+     * that is in CPMS, as per OLCS-11260.
+     *
+     * @see PaymentDb\Validator\Address in http://gitlab.clb.npm/cpms/payment-db/
+     * @todo this will fail (incorrectly) on short postcodes without spaces, e.g. 'L23SW'
      *
      * @param string $value
      * @return bool
      */
     public function isValid($value)
     {
-        // strip whitespace
-        $value = preg_replace('/\s+/', '', $value);
+        $length = strlen($value);
 
-        // uppercase
-        $value = strtoupper($value);
+        if ($length < self::POSTCODE_MIN_LENGTH || $length > self::POSTCODE_MAX_LENGTH) {
+            $this->error(self::POSTCODE_BAD_LENGTH);
 
-        foreach ($this->patterns as $pattern) {
-            if (preg_match($pattern, $value)) {
+            return false;
+        }
+
+        $postcode = strtoupper(str_replace(' ', '', $value));
+        foreach ($this->postcodeFormats as $format) {
+            if (preg_match("/^" . $format . "$/", $postcode)) {
                 return true;
             }
         }
+        $this->error(self::POSTCODE_INVALID_FORMAT);
 
-        $this->error('invalid');
         return false;
     }
 }
--- test/src/Validators/PostcodeTest.php
+++ test/src/Validators/PostcodeTest.php
@@ -27,16 +27,11 @@ class PostcodeTest extends PHPUnit_Framework_TestCase
     /**
      * @dataProvider isValidProvider
      */
-    public function testIsValid($value, $expected)
+    public function testIsValid($value, $expected, $expectedErrors = [])
     {
         $this->assertEquals($expected, $this->sut->isValid($value));
 
-        if ($expected === false) {
-            $this->assertEquals(
-                ['invalid' => 'Invalid postcode'],
-                $this->sut->getMessages()
-            );
-        }
+        $this->assertEquals($expectedErrors, $this->sut->getMessages());
     }
 
     public function isValidProvider()
@@ -47,10 +42,23 @@ class PostcodeTest extends PHPUnit_Framework_TestCase
             ['ls96NF', true],
             [' ls96NF', true],
             ['ls99 6NF', true],
-            ['ls9336NF', false],
+            [
+                'ls9336NF',
+                false,
+                ['invalidPostcodeFormat' => 'Invalid postcode format']
+            ],
             ['W1A4AA', true],
             ['GIR 0AA', true],
-            ['not a postcode', false],
+            [
+                'not a postcode',
+                false,
+                ['postcodeBadLength' => 'Postcode must be either 6, 7 or 8 characters long and start with a letter'],
+            ],
+            ['GIR 0AA', true],
+            ['L2 3SW', true],
+
+            // @todo this *should* be a valid postcode
+            // ['L23SW', true],
         ];
     }
 }
