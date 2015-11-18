66893abbf0f2ea7ec027cb96a495bbb6c2283f1c
Rob Caiger <rob@clocal.co.uk>
2015-11-18 15:38:11 +0000

    Removed unused use

--- src/Query/VariationOperatingCentre/VariationOperatingCentre.php
+++ src/Query/VariationOperatingCentre/VariationOperatingCentre.php
@@ -7,7 +7,6 @@
  */
 namespace Dvsa\Olcs\Transfer\Query\VariationOperatingCentre;
 
-use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 
