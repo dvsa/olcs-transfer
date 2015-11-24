bc6c4f9fe7748a57f0a31f1106d1ad0fd0c5ddc9
Alex P <alex.peshkov@valtech.co.uk>
2015-11-24 17:20:18 +0000

    OLCS-11054 address data service migration started

---
+++ src/Query/Address/GetAddress.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Get address for uprn
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+namespace Dvsa\Olcs\Transfer\Query\Address;
+
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+
+/**
+ * @Transfer\RouteName("backend/address")
+ */
+final class GetAddress extends AbstractQuery
+{
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $uprn;
+
+    /**
+     * Get UPRN
+     *
+     * @return int
+     */
+    public function getUprn()
+    {
+        return $this->uprn;
+    }
+}
