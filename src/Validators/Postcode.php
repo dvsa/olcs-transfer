67bc4e4817ec8f7e2c75e0f857e039d23ecf377d
Dan Eggleston <dan@stolenegg.com>
2015-11-30 16:55:32 +0000

    OLCS-11382 update postcode validator and tests

--- src/Validators/Postcode.php
+++ src/Validators/Postcode.php
@@ -20,10 +20,14 @@ class Postcode extends AbstractValidator
     const POSTCODE_MAX_LENGTH = 8;
     const POSTCODE_BAD_LENGTH     = 'postcodeBadLength';
     const POSTCODE_INVALID_FORMAT = 'invalidPostcodeFormat';
+    const POSTCODE_IS_EMPTY       = 'isEmpty';
+    const POSTCODE_TOO_LONG       = 'stringLengthTooLong';
 
     protected $messageTemplates = [
         self::POSTCODE_BAD_LENGTH     => 'postcode.validation.postcodeBadLength',
         self::POSTCODE_INVALID_FORMAT => 'postcode.validation.invalidPostcodeFormat',
+        self::POSTCODE_IS_EMPTY       => 'postcode.validation.isEmpty',
+        self::POSTCODE_TOO_LONG       => 'postcode.validation.stringLengthTooLong',
     ];
 
     protected $postcodeFormats = [
@@ -33,22 +37,78 @@ class Postcode extends AbstractValidator
     ];
 
     /**
+     * @var bool
+     */
+    protected $allowEmpty = false;
+
+    /**
+     * @param boolean $allowEmpty
+     */
+    public function setAllowEmpty($allowEmpty)
+    {
+        $this->allowEmpty = $allowEmpty;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function allowEmpty()
+    {
+        return $this->allowEmpty;
+    }
+
+    /**
+     * @param array $options
+     * @return AbstractValidator
+     */
+    public function setOptions($options = [])
+    {
+        if (isset($options['allow_empty'])) {
+            $this->setAllowEmpty($options['allow_empty']);
+        }
+
+        return parent::setOptions($options);
+    }
+
+    /**
      * Check if a postcode is valid. This re-implements the validation logic
      * that is in CPMS, as per OLCS-11260.
      *
      * @see PaymentDb\Validator\Address in http://gitlab.clb.npm/cpms/payment-db/
-     * @todo this will fail (incorrectly) on short postcodes without spaces, e.g. 'L23SW'
      *
      * @param string $value
+     * @param array $context
      * @return bool
      */
-    public function isValid($value)
+    public function isValid($value, $context = null)
     {
+        if (empty($value) && $this->allowEmpty()) {
+            return true;
+        }
+
+        if (isset($context['countryCode']) && $context['countryCode'] === 'GB') {
+            return $this->validateUkPostcode($value, $context);
+        }
+
+        return $this->validateNonUkPostcode($value, $context);
+    }
+
+    /**
+     * UK postcode cannot usually be empty, must be 5-8 characters and must
+     * match a predefined format
+     */
+    private function validateUkPostcode($value, $context)
+    {
+        if (empty($value)) {
+            $this->error(self::POSTCODE_IS_EMPTY);
+            return false;
+        }
+
         $length = strlen($value);
 
         if ($length < self::POSTCODE_MIN_LENGTH || $length > self::POSTCODE_MAX_LENGTH) {
             $this->error(self::POSTCODE_BAD_LENGTH);
-
             return false;
         }
 
@@ -61,5 +121,20 @@ class Postcode extends AbstractValidator
         $this->error(self::POSTCODE_INVALID_FORMAT);
 
         return false;
+    }
+
+    /**
+     * Non-UK postcode must not exceed maximum field length
+     */
+    private function validateNonUkPostcode($value, $context)
+    {
+        $length = strlen($value);
+
+        if ($length > self::POSTCODE_MAX_LENGTH) {
+            $this->error(self::POSTCODE_TOO_LONG);
+            return false;
+        }
+
+        return true;
     }
 }
--- test/src/Validators/PostcodeTest.php
+++ test/src/Validators/PostcodeTest.php
@@ -27,9 +27,11 @@ class PostcodeTest extends PHPUnit_Framework_TestCase
     /**
      * @dataProvider isValidProvider
      */
-    public function testIsValid($value, $expected, $expectedErrors = [])
+    public function testIsValid($value, $context, $expected, $expectedErrors = [], $options = [])
     {
-        $this->assertEquals($expected, $this->sut->isValid($value));
+        $this->sut->setOptions($options);
+
+        $this->assertEquals($expected, $this->sut->isValid($value, $context));
 
         $this->assertEquals($expectedErrors, $this->sut->getMessages());
     }
@@ -37,27 +39,53 @@ class PostcodeTest extends PHPUnit_Framework_TestCase
     public function isValidProvider()
     {
         return [
-            ['LS9 6NF', true],
-            ['ls9 6nf', true],
-            ['ls96NF', true],
-            [' ls96NF', true],
-            ['ls99 6NF', true],
+            ['LS9 6NF', ['countryCode' => 'GB'], true],
+            ['ls9 6nf', ['countryCode' => 'GB'], true],
+            ['ls96NF', ['countryCode' => 'GB'], true],
+            [' ls96NF', ['countryCode' => 'GB'], true],
+            ['ls99 6NF', ['countryCode' => 'GB'], true],
             [
                 'ls9336NF',
+                ['countryCode' => 'GB'],
                 false,
                 ['invalidPostcodeFormat' => 'postcode.validation.invalidPostcodeFormat']
             ],
-            ['W1A4AA', true],
-            ['GIR 0AA', true],
+            ['W1A4AA', ['countryCode' => 'GB'], true],
+            ['GIR 0AA', ['countryCode' => 'GB'], true],
             [
                 'not a postcode',
+                ['countryCode' => 'GB'],
                 false,
                 ['postcodeBadLength' => 'postcode.validation.postcodeBadLength'],
             ],
-            ['GIR 0AA', true],
-            ['L2 3SW', true],
+            ['GIR 0AA', ['countryCode' => 'GB'], true],
+            ['L2 3SW', ['countryCode' => 'GB'], true],
 
-            ['L23SW', true],
+            ['L23SW', ['countryCode' => 'GB'], true],
+
+            ['f0reign', ['countryCode' => 'US'], true],
+            [
+                'f0reign too long',
+                ['countryCode' => 'US'],
+                false,
+                ['stringLengthTooLong' => 'postcode.validation.stringLengthTooLong'],
+            ],
+            ['', ['countryCode' => 'US'], true],
+            ['', [], true],
+            [
+                '',
+                ['countryCode' => 'GB'],
+                false,
+                ['isEmpty' => 'postcode.validation.isEmpty'],
+            ],
+            [
+                '',
+                ['countryCode' => 'GB'],
+                true,
+                [],
+                ['allow_empty' => true],
+            ],
+
         ];
     }
 }
