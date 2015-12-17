fcd87c3b57d1c4b4e3e13cb312b5be098ecac612
Shaun Lizzio <shaun@lizzio.co.uk>
2015-12-17 13:58:50 +0000

    Replaced abstract class as no longer updating record

--- src/Command/Irfo/ResetIrfoPsvAuth.php
+++ src/Command/Irfo/ResetIrfoPsvAuth.php
@@ -7,11 +7,13 @@ namespace Dvsa\Olcs\Transfer\Command\Irfo;
 
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
 
 /**
  * @Transfer\RouteName("backend/irfo/psv-auth/single/reset")
  * @Transfer\Method("PUT")
  */
-final class ResetIrfoPsvAuth extends UpdateIrfoPsvAuth
+final class ResetIrfoPsvAuth extends AbstractCommand
 {
+    use Identity;
 }
