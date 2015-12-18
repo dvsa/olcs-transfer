1c88609438981870de40d675b21d4cd735b688a8
Alex P <alex.peshkov@valtech.co.uk>
2015-12-18 12:30:35 +0000

    OLCS-11643 TrafficArea data service migration

---
+++ config/backend-routes/traffic-area/traffic-area.php
@@ -0,0 +1,20 @@
+<?php
+
+use Dvsa\Olcs\Transfer\Command;
+use Dvsa\Olcs\Transfer\Query;
+use Dvsa\Olcs\Transfer\Router\CommandConfig;
+use Dvsa\Olcs\Transfer\Router\QueryConfig;
+use Dvsa\Olcs\Transfer\Router\RouteConfig;
+
+return [
+    'traffic-area' => [
+        'type' => 'Segment',
+        'options' => [
+            'route' => 'traffic-area[/]',
+        ],
+        'may_terminate' => false,
+        'child_routes' => [
+            'GET' => QueryConfig::getConfig(Query\TrafficArea\TrafficAreaList::class),
+        ]
+    ],
+];
