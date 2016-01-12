a02c428e41a0b52357d5f47ad9dbb5d4bdd0d23b
Alex P <alex.peshkov@valtech.co.uk>
2016-01-12 16:56:37 +0000

    OLCS-11634 pagination added to query

--- src/Query/Cases/PiVenue/PiVenueList.php
+++ src/Query/Cases/PiVenue/PiVenueList.php
@@ -5,12 +5,15 @@ namespace Dvsa\Olcs\Transfer\Query\Cases\PiVenue;
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\FieldType\Traits\TrafficArea;
+use Dvsa\Olcs\Transfer\Query\PagedQueryInterface;
+use Dvsa\Olcs\Transfer\Query\PagedTrait;
 
 /**
  * Class PiVenueList
  * @Transfer\RouteName("backend/venue-list")
  */
-class PiVenueList extends AbstractQuery
+class PiVenueList extends AbstractQuery implements  PagedQueryInterface
 {
-    use TrafficArea;
+    use TrafficArea,
+        PagedTrait;
 }
