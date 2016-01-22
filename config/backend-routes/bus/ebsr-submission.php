c6d0be4efecc0946ed6ea8541c60434500ce6cad
Shaun Lizzio <shaun@lizzio.co.uk>
2016-01-22 12:14:37 +0000

    Added new Query for EbsrSubmissionList

---
+++ config/backend-routes/bus/ebsr-submission.php
@@ -0,0 +1,20 @@
+<?php
+
+use Dvsa\Olcs\Transfer\Command;
+use Dvsa\Olcs\Transfer\Query;
+use Dvsa\Olcs\Transfer\Router\CommandConfig;
+use Dvsa\Olcs\Transfer\Router\QueryConfig;
+use Dvsa\Olcs\Transfer\Router\RouteConfig;
+
+return [
+    'ebsr-submission' => [
+        'type' => 'Segment',
+        'options' => [
+            'route' => 'ebsr-submission[/]',
+        ],
+        'may_terminate' => false,
+        'child_routes' => [
+            'GET' => QueryConfig::getConfig(Query\Bus\Ebsr\EbsrSubmissionList::class)
+        ]
+    ]
+];
