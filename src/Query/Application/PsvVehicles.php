50faf54a3b8fdf97a84b344448f7da824c266545
Alex P <alex.peshkov@valtech.co.uk>
2016-02-15 14:34:05 +0000

    OLCS-11948 PSV Vehicle page pagination

--- src/Query/Application/PsvVehicles.php
+++ src/Query/Application/PsvVehicles.php
@@ -11,13 +11,16 @@ use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
 use Dvsa\Olcs\Transfer\Query\CachableQueryInterface;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Query\PagedQueryInterface;
+use Dvsa\Olcs\Transfer\Query\PagedTrait;
 
 /**
  * @Transfer\RouteName("backend/application/single/psv-vehicles")
  */
-class PsvVehicles extends AbstractQuery implements CachableQueryInterface
+class PsvVehicles extends AbstractQuery implements CachableQueryInterface, PagedQueryInterface
 {
-    use Identity;
+    use Identity,
+        PagedTrait;
 
     /**
      * @Transfer\Filter({"name":"Zend\Filter\Boolean"})
