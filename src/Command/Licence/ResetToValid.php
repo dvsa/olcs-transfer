8ed5b911f7018a9d328d32aa134599ecca0dabcc
Shaun Lizzio <shaun@lizzio.co.uk>
2016-03-08 09:08:10 +0000

    Added reasons to relevant DTOs. Part complete. Moving story to blocked

--- src/Command/Licence/ResetToValid.php
+++ src/Command/Licence/ResetToValid.php
@@ -9,6 +9,7 @@ namespace Dvsa\Olcs\Transfer\Command\Licence;
 
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+use Dvsa\Olcs\Transfer\FieldType;
 
 /**
  * @Transfer\RouteName("backend/licence/single/decisions/reset-to-valid")
@@ -16,12 +17,15 @@ use Dvsa\Olcs\Transfer\Command\AbstractCommand;
  */
 final class ResetToValid extends AbstractCommand
 {
+    use FieldType\Traits\ReasonsOptional;
+
     /**
      * @Transfer\Filter({"name":"Zend\Filter\Digits"})
      * @Transfer\Validator({"name":"Zend\Validator\Digits"})
      * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
      */
     protected $id;
+
 
     /**
      * @return mixed
