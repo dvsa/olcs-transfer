8f8c2eb75df3220a6f7953fed1d6d666acc30f90
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-03-08 14:55:31 +0000

    Query to retreive a single Serious infringement

---
+++ src/Query/Cases/Si/Si.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Si
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Dvsa\Olcs\Transfer\Query\Cases\Si;
+
+use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Query\CachableQueryInterface;
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+
+/**
+ * @Transfer\RouteName("backend/si/single")
+ */
+class Si extends AbstractQuery implements CachableQueryInterface
+{
+    use Identity;
+}
