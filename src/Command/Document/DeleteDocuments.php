74495801dc8873b48540ad9f28f337e91610db77
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-06-03 15:41:06 +0100

    Removed use statement which wasn't used

--- src/Command/Document/DeleteDocuments.php
+++ src/Command/Document/DeleteDocuments.php
@@ -9,7 +9,6 @@ namespace Dvsa\Olcs\Transfer\Command\Document;
 
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\Command\AbstractCommand;
-use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
 
 /**
  * @Transfer\RouteName("backend/document")
