01b429ae520e1d5422ac467c74e631eb9662a68e
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2016-07-20 15:06:11 +0100

    OLCS-13087/Fix2: move common Delete command functionality  to abstract class; change application and licence with traits;

--- src/Command/Licence/UpdateCompanySubsidiary.php
+++ src/Command/Licence/UpdateCompanySubsidiary.php
@@ -1,36 +1,18 @@
 <?php
 
-/**
- * Update Company Subsidiary
- *
- * @author Rob Caiger <rob@clocal.co.uk>
- */
 namespace Dvsa\Olcs\Transfer\Command\Licence;
 
 use Dvsa\Olcs\Transfer\Command\Lva\AbstractUpdateCompanySubsidiary;
+use Dvsa\Olcs\Transfer\FieldType\Traits\Licence;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 
 /**
+ * @author Rob Caiger <rob@clocal.co.uk>
+ *         
  * @Transfer\RouteName("backend/licence/named-single/company-subsidiary/single")
  * @Transfer\Method("PUT")
  */
 final class UpdateCompanySubsidiary extends AbstractUpdateCompanySubsidiary
 {
-    /**
-     * @var int
-     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
-     */
-    protected $licence;
-
-    /**
-     * Return licence identifier
-     *
-     * @return int|null
-     */
-    public function getLicence()
-    {
-        return $this->licence;
-    }
+    use Licence;
 }
---
+++ test/src/Command/Lva/AbstractDeleteCompanySubsidiaryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Dvsa\OlcsTest\Transfer\Command;
+
+use Dvsa\Olcs\Transfer\Command\Lva\AbstractDeleteCompanySubsidiary;
+
+/**
+ * @covers Dvsa\Olcs\Transfer\Command\Lva\AbstractDeleteCompanySubsidiary
+ */
+class AbstractDeleteCompanySubsidiaryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetSet()
+    {
+        $command = TestAbstractDeleteCompanySubsidiary::create(
+            [
+                'ids' => [111, 222, 333],
+            ]
+        );
+
+        static::assertEquals([111, 222, 333], $command->getIds());
+    }
+}
+
+/**
+ * Class for testing Abstract Class
+ */
+class TestAbstractDeleteCompanySubsidiary extends AbstractDeleteCompanySubsidiary
+{
+}
