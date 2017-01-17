c6546de0f36e11c9fb7a3f028a262b12537c19c9
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2017-01-17 15:31:43 +0000

    OLCS-14966: add field in Queries; unit tests;

--- src/Query/Category/GetList.php
+++ src/Query/Category/GetList.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * Get a list of Category's
- *
- * @author Mat Evans <mat.evans@valtech.co.uk>
- */
 namespace Dvsa\Olcs\Transfer\Query\Category;
 
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
@@ -21,35 +16,70 @@ class GetList extends AbstractQuery implements
     use \Dvsa\Olcs\Transfer\Query\OrderedTrait;
 
     /**
+     * @var string
      * @Transfer\Validator({"name":"Dvsa\Olcs\Transfer\Validators\YesNo"})
      * @Transfer\Optional
      */
     protected $isTaskCategory;
 
     /**
+     * @var string
      * @Transfer\Validator({"name":"Dvsa\Olcs\Transfer\Validators\YesNo"})
      * @Transfer\Optional
      */
     protected $isDocCategory;
 
     /**
+     * @var string
      * @Transfer\Validator({"name":"Dvsa\Olcs\Transfer\Validators\YesNo"})
      * @Transfer\Optional
      */
     protected $isScanCategory;
 
+    /**
+     * @var string
+     * @Transfer\Validator({"name":"Dvsa\Olcs\Transfer\Validators\YesNo"})
+     * @Transfer\Optional
+     */
+    protected $isOnlyWithItems;
+
+    /**
+     * Get Is Task Category
+     *
+     * @return string
+     */
     public function getIsTaskCategory()
     {
         return $this->isTaskCategory;
     }
 
+    /**
+     * Get is Doc Category
+     *
+     * @return string
+     */
     public function getIsDocCategory()
     {
         return $this->isDocCategory;
     }
 
+    /**
+     * Get is Scan Category
+     *
+     * @return string
+     */
     public function getIsScanCategory()
     {
         return $this->isScanCategory;
+    }
+
+    /**
+     * Is Take only categories with Items
+     *
+     * @return string
+     */
+    public function getIsOnlyWithItems()
+    {
+        return $this->isOnlyWithItems;
     }
 }
