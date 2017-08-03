2253e68d32ff0a396904e26a9c0b4f4c24d2c607
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-08-03 10:04:44 +0100

    OLCS-17144 data retention rule list transfer object

---
+++ src/Query/DataRetention/RuleList.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dvsa\Olcs\Transfer\Query\DataRetention;
+
+use Dvsa\Olcs\Transfer\Query\OrderedQueryInterface;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Query\PagedQueryInterface;
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Query\OrderedTrait;
+use Dvsa\Olcs\Transfer\Query\PagedTrait;
+
+/**
+ * @Transfer\RouteName("backend/data-retention")
+ */
+final class RuleList extends AbstractQuery implements
+    PagedQueryInterface,
+    OrderedQueryInterface
+{
+    use PagedTrait;
+    use OrderedTrait;
+}
