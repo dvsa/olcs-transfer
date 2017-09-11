17b00d6115bb1b1c2b55a948c9ebe66dcb063436
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-09-11 11:06:16 +0100

    OLCS-17159 new command to mark for deletion

---
+++ src/Command/DataRetention/MarkToDeleteRecord.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Update Document Links
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace Dvsa\Olcs\Transfer\Command\Document;
+
+use Dvsa\Olcs\Transfer\FieldType\Traits\ApplicationOptional;
+use Dvsa\Olcs\Transfer\FieldType\Traits\BusRegOptional;
+use Dvsa\Olcs\Transfer\FieldType\Traits\CasesOptional;
+use Dvsa\Olcs\Transfer\FieldType\Traits\LicenceOptional;
+use Dvsa\Olcs\Transfer\FieldType\Traits\TransportManagerOptional;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
+
+/**
+ * @Transfer\RouteName("backend/data-retention/action-confirmation")
+ * @Transfer\Method("PUT")
+ */
+final class MarkToDeleteRecord extends AbstractCommand
+{
+    use Identity;
+}
