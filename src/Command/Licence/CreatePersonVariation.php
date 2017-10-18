b7e76e4827f82c061ef8b4161d28ac0a56a51b24
Shaun Hare <shaun.hare@dvsa.gov.uk>
2017-10-18 14:52:18 +0100

    Person command

---
+++ src/Command/Licence/CreatePersonVariation.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Person Variation
+ * @author Shaun Hare - shaun.hare@dvsa.gov.uk
+ */
+
+namespace Dvsa\Olcs\Transfer\Command\Licence;
+
+use Dvsa\Olcs\Transfer\Command\AbstractPeople;
+
+/**
+ * Class CreatePersonVariation
+ * @package Dvsa\Olcs\Transfer\Command\Licence
+ */
+final class CreatePersonVariation extends AbstractPeople
+{
+
+
+}
