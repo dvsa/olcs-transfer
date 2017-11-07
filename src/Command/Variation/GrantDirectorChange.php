a67b13d3d15c4beb76b5ed9ae11fa57eeb9de5b8
Shaun Hare <shaun.hare@dvsa.gov.uk>
2017-11-07 11:04:36 +0000

    wip

--- src/Command/Variation/GrantDirectorChange.php
+++ src/Command/Variation/GrantDirectorChange.php
@@ -3,8 +3,12 @@
 
 namespace Dvsa\Olcs\Transfer\Command\Variation;
 
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+
 /**
- * @Transfer\RouteName("backend/variation/single/grantDirectorChange")
+ * @Transfer\RouteName("backend/variation/single/grant-director-change")
  * @Transfer\Method("PUT")
  */
 class GrantDirectorChange extends AbstractCommand
