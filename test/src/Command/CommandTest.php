5b6057872e6d9312e75e8ec98fbf915ed6b81879
Richard Ward <richard.ward@bjss.com>
2017-12-11 13:26:06 +0000

    Add a ComamndTest trait for testing command validation
    OLCS-18191

---
+++ test/src/Command/CommandTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dvsa\OlcsTest\Transfer\Command;
+
+use Dvsa\Olcs\Transfer\Util\Annotation\AnnotationBuilder;
+use Dvsa\OlcsTest\Transfer\DtoTest;
+use Zend\Stdlib\ArraySerializableInterface;
+
+trait CommandTest
+{
+    use DtoTest;
+
+    protected function createDtoContainer(ArraySerializableInterface $dto)
+    {
+        return (new AnnotationBuilder())->createCommand($dto);
+    }
+}
