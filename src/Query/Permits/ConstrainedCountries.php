2bc32d4e77cf08016c4db662114fd0d8527efb98
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-06-01 17:29:22 +0100

    added country constraints query and api endpoint

---
+++ src/Query/Permits/ConstrainedCountries.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Sectors
+ *
+ * @author Tonci Vidovic <tonci.vidovic@capgemini.com>
+ */
+namespace Dvsa\Olcs\Transfer\Query\Permits;
+
+//use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
+use Dvsa\Olcs\Transfer\Query\CachableShortTermQueryInterface;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+
+/**
+ * @Transfer\RouteName("backend/permits/constrained-countries")
+ */
+class ConstrainedCountries extends AbstractQuery implements CachableShortTermQueryInterface
+{
+    //use Identity;
+}
