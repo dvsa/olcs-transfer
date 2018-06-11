9fba227b57380b329b8573c73c509a1a2f33776e
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-06-11 16:07:38 +0100

    olcs-20199 add DTOs for create/update/deletion of feature toggles

---
+++ config/backend-routes/feature-toggle/feature-toggle.php
@@ -0,0 +1,28 @@
+<?php
+
+use Dvsa\Olcs\Transfer\Command;
+use Dvsa\Olcs\Transfer\Query;
+use Dvsa\Olcs\Transfer\Router\CommandConfig;
+use Dvsa\Olcs\Transfer\Router\QueryConfig;
+use Dvsa\Olcs\Transfer\Router\RouteConfig;
+
+return [
+    'feature-toggle' => [
+        'type' => 'Segment',
+        'options' => [
+            'route' => 'feature-toggle[/]',
+        ],
+        'may_terminate' => false,
+        'child_routes' => [
+            'single' => RouteConfig::getSingleConfig(
+                [
+                    'GET' => QueryConfig::getConfig(Query\FeatureToggle\ById::class),
+                    'PUT' => CommandConfig::getPutConfig(Command\FeatureToggle\Update::class),
+                    'DELETE' => CommandConfig::getDeleteConfig(Command\FeatureToggle\Delete::class),
+                ]
+            ),
+            'GET' => QueryConfig::getConfig(Query\FeatureToggle\GetList::class),
+            'POST' => CommandConfig::getPostConfig(Command\FeatureToggle\Create::class),
+        ]
+    ],
+];
