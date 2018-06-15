853f3583304f3753df5062b2b205a52b1ef739b5
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-06-15 13:48:29 +0100

    olcs-20199 added a route to check whether a a feature toggle is enabled

---
+++ src/Query/FeatureToggle/IsEnabled.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Get a single feature toggle by id
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Dvsa\Olcs\Transfer\Query\FeatureToggle;
+
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+
+/**
+ * @Transfer\RouteName("backend/feature-toggle/check")
+ */
+class IsEnabled extends AbstractQuery
+{
+    /**
+     * @Transfer\ArrayInput
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options":{"max":255}})
+     */
+    protected $ids = [];
+
+    public function getIds(): array
+    {
+        return $this->ids;
+    }
+}
