069fd667ae8fbee287f92aff5090325772e23d9f
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-07-13 13:36:58 +0100

    olcs-20554 olcs-20618 add query to check organisation is eligible for permits

--- src/Query/Organisation/EligibleForPermits.php
+++ src/Query/Organisation/EligibleForPermits.php
@@ -2,13 +2,14 @@
 
 namespace Dvsa\Olcs\Transfer\Query\Organisation;
 
-use Dvsa\Olcs\Transfer\Query\CachableShortTermQueryInterface;
+use Dvsa\Olcs\Transfer\FieldType\Traits\IdentityOptional;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 
 /**
- * @Transfer\RouteName("backend/organisation/eligible-for-permits")
+ * @Transfer\RouteName("backend/organisation/single/eligible-for-permits")
  */
-class EligibleForPermits extends AbstractQuery implements CachableShortTermQueryInterface
+class EligibleForPermits extends AbstractQuery
 {
+    use IdentityOptional;
 }
