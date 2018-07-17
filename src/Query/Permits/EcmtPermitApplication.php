96d1d869192a579f40bc3c4e592c0dda48860a08
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-07-17 14:10:25 +0100

    dashboard page tables and queries added

---
+++ src/Query/Permits/EcmtPermitApplication.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * ECMT Applications
+ *
+ * @author Tonci Vidovic <tonci.vidovic@capgemini.com>
+ */
+namespace Dvsa\Olcs\Transfer\Query\Permits;
+
+//use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
+use Dvsa\Olcs\Transfer\Query\CachableShortTermQueryInterface;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+
+/**
+ * @Transfer\RouteName("backend/permits/ecmt-permit-application")
+ */
+class EcmtPermitApplication extends AbstractQuery implements CachableShortTermQueryInterface
+{
+    //use Identity;
+}
