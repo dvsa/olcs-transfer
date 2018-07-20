7cb4ee8563556aae898420394e7e285d163b2a01
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-07-20 16:50:22 +0100

    UpdateEcmtPermitApplication command added

--- config/backend-routes.config.php
+++ config/backend-routes.config.php
@@ -75,6 +75,7 @@ $routes = [
                 'child_routes' => [
                   'GET' => QueryConfig::getConfig(Query\Permits\EcmtPermitApplication::class),
                   'POST' => CommandConfig::getPostConfig(Command\Permits\CreateEcmtPermitApplication::class),
+                  'PUT' => CommandConfig::getPutConfig(Command\Permits\UpdateEcmtPermitApplication::class),
                 ]
               ],
               'single' => [
