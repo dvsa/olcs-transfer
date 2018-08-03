cc46723fe8ff191d82a72974a4b4942fb5ee1169
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-08-03 23:39:59 +0100

    added array to fee query so able to get more than one feeType from DB

--- src/Query/Permits/EcmtPermitFees.php
+++ src/Query/Permits/EcmtPermitFees.php
@@ -17,5 +17,21 @@ use Dvsa\Olcs\Transfer\Query\CachableShortTermQueryInterface;
  */
 class EcmtPermitFees extends AbstractQuery
 {
+    /**
+     * @Transfer\ArrayInput
+     * @Transfer\ArrayFilter({"name":"Dvsa\Olcs\Transfer\Filter\FilterEmptyItems"})
+     * @Transfer\ArrayFilter({"name":"Dvsa\Olcs\Transfer\Filter\UniqueItems"})
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options":{"min": 1}})
+     */
+    protected $productReferences;
+
+    /**
+     * @return mixed
+     */
+    public function getProductReferences()
+    {
+        return $this->productReferences;
+    }
 
 }
