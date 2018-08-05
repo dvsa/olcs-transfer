336a7a1fab328c9b6f759eea46a523550a0d70c4
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-08-05 14:58:39 +0100

    fix trips validation to between 1 and 999999

--- src/FieldType/Traits/EcmtTrips.php
+++ src/FieldType/Traits/EcmtTrips.php
@@ -9,9 +9,8 @@ trait EcmtTrips
 {
     /**
      * @var int
-     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
      * @Transfer\Validator({"name":"Zend\Validator\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     * @Transfer\Validator({"name":"Zend\Validator\Between", "options": {"min": 1, "max": 999999}})
      */
     protected $ecmtTrips;
 
