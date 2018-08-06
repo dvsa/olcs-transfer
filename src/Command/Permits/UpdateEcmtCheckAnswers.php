18cc18a709cc1d186ceda93a293dcc9d22f06c81
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-08-06 02:01:17 +0100

    olcs-20547 add route for updating check answers

---
+++ src/Command/Permits/UpdateEcmtCheckAnswers.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Update check answers
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Dvsa\Olcs\Transfer\Command\Permits;
+
+use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+
+/**
+ * @Transfer\RouteName("backend/permits/ecmt-permits-update-answers")
+ * @Transfer\Method("PUT")
+ */
+final class UpdateEcmtCheckAnswers extends AbstractCommand
+{
+    use Identity;
+
+    /**
+     * @var bool
+     * @Transfer\Filter({"name":"Zend\Filter\Boolean"})
+     */
+    protected $checkAnswers;
+
+    /**
+     * @return int
+     */
+    public function getCheckAnswers()
+    {
+        return $this->checkAnswers;
+    }
+}
