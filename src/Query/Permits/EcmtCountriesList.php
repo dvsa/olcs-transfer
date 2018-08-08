c30ebaa855cb9e2a0878e55655d863c083372187
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-08-08 17:05:34 +0100

    OLCS-20884 added query and trait for fetching ecmt countries

--- src/Query/Permits/EcmtCountriesList.php
+++ src/Query/Permits/EcmtCountriesList.php
@@ -1,20 +1,21 @@
 <?php
 
 /**
- * Sectors
+ * ECMT Countries
  *
  * @author Tonci Vidovic <tonci.vidovic@capgemini.com>
  */
 namespace Dvsa\Olcs\Transfer\Query\Permits;
 
+use Dvsa\Olcs\Transfer\FieldType\Traits\IsEcmtState;
 use Dvsa\Olcs\Transfer\Query\CachableShortTermQueryInterface;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 
 /**
- * @Transfer\RouteName("backend/permits/constrained-countries")
+ * @Transfer\RouteName("backend/permits/ecmt-countries")
  */
-class ConstrainedCountries extends AbstractQuery implements CachableShortTermQueryInterface
+class EcmtCountriesList extends AbstractQuery implements CachableShortTermQueryInterface
 {
-
+    use IsEcmtState;
 }
