47a08ce82aef67d21c81afb090de9305b6c0cbd4
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-08-09 13:56:17 +0100

    changed save commands to allow equivalent of empty value so that return to overview criteria can be met

--- src/FieldType/Traits/Cabotage.php
+++ src/FieldType/Traits/Cabotage.php
@@ -12,7 +12,6 @@ trait Cabotage
      * @var int
      * @Transfer\Filter({"name":"Zend\Filter\Digits"})
      * @Transfer\Validator({"name":"Zend\Validator\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
      */
     protected $cabotage;
 
