a8b610a6c203976ba0cdbbb961a85fb410906d74
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-08-09 10:04:09 +0100

    OLCS-20884 added query unit test

---
+++ test/src/Query/Permits/EcmtCountriesListTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Dvsa\OlcsTest\Transfer\Query\Permits;
+
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Dvsa\Olcs\Transfer\Query\Permits\EcmtCountriesList;
+
+/**
+ * @covers \Dvsa\Olcs\Transfer\Query\Permits\EcmtCountriesList
+ */
+
+
+class EcmtCountriesListTest extends MockeryTestCase
+{
+    public function testEcmtCountriesList()
+    {
+        $sut = EcmtCountriesList::create(
+          [
+            'isEcmtState' => 1
+          ]
+        );
+
+        static::assertEquals(1, $sut->getIsEcmtState());
+    }
+}
