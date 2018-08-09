3b002dc5a1d07a4614ba9a4cb447d359efa2a932
Scott Callaway <github@scottcallaway.co.uk>
2018-08-09 12:19:26 +0100

    Add Ordering Trait to EcmtPermitApplication Query

--- src/Query/Permits/EcmtPermitApplication.php
+++ src/Query/Permits/EcmtPermitApplication.php
@@ -6,6 +6,7 @@
  * @author Tonci Vidovic <tonci.vidovic@capgemini.com>
  */
 namespace Dvsa\Olcs\Transfer\Query\Permits;
+use Dvsa\Olcs\Transfer\Query\OrderedTraitOptional;
 
 use Dvsa\Olcs\Transfer\Query\CachableShortTermQueryInterface;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
@@ -16,5 +17,5 @@ use Dvsa\Olcs\Transfer\Query\AbstractQuery;
  */
 class EcmtPermitApplication extends AbstractQuery implements CachableShortTermQueryInterface
 {
-
+    use OrderedTraitOptional;
 }
