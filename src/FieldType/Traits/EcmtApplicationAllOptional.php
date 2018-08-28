58ad4dc2f9abc41424488a545feca1d12a8e7049
Andy Newton <andrew.newton@capgemini.com>
2018-08-28 17:19:01 +0100

    OLCS-19268 - Typo in validator inarray

--- src/FieldType/Traits/EcmtApplicationAllOptional.php
+++ src/FieldType/Traits/EcmtApplicationAllOptional.php
@@ -167,7 +167,7 @@ trait EcmtApplicationAllOptional
     /**
      * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
      * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options":{"min":1, "max":32}})
-     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"ecmt_permit_awaiting", "ecmt_permit_cancelled", "ecmt_permit_issued", "ecmt_permit_nys", "ecmt_permit_uc", "ecmt_permit_unsuccessful", "cmt_permit_withdrawn"}}})
+     * @Transfer\Validator({"name":"Zend\Validator\InArray", "options": {"haystack": {"ecmt_permit_awaiting", "ecmt_permit_cancelled", "ecmt_permit_issued", "ecmt_permit_nys", "ecmt_permit_uc", "ecmt_permit_unsuccessful", "ecmt_permit_withdrawn"}}})
      * @Transfer\Optional
      */
     public $status;
