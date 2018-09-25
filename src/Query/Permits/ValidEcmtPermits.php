3d57060bf99a3f1a5e0c54e0c25dab4d3ed37a4b
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-09-25 13:40:24 +0100

    OLCS-21385 review script output applied

--- src/Query/Permits/ValidEcmtPermits.php
+++ src/Query/Permits/ValidEcmtPermits.php
@@ -13,7 +13,6 @@ use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
 use Dvsa\Olcs\Transfer\Query\PagedTrait;
 
-
 /**
  * @Transfer\RouteName("backend/permits/valid-ecmt")
  */
