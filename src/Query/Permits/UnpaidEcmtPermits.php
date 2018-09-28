cf033f89a5dfec87b108e7988dae1c0f67c1847d
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-09-28 20:48:12 +0100

    OLCS-21760 new page for displaying unpaid permits

---
+++ src/Query/Permits/UnpaidEcmtPermits.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Get unpaid permits by status
+ *
+ * @author Tonci Vidovic <tonci.vidovic@capgemini.com>
+ */
+namespace Dvsa\Olcs\Transfer\Query\Permits;
+
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Query\PagedQueryInterface;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
+use Dvsa\Olcs\Transfer\Query\PagedTrait;
+
+/**
+ * @Transfer\RouteName("backend/permits/unpaid-ecmt")
+ */
+class UnpaidEcmtPermits extends AbstractQuery implements PagedQueryInterface
+{
+    use Identity;
+    use PagedTrait;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     */
+    protected $status = null;
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
