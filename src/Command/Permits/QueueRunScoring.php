f755a92986cd9ef8aab1c6dace9c2c9d68b2dd6b
Jonathan Thomas <jonathan@opalise.co.uk>
2018-10-12 18:14:47 +0100

    OLCS-21533 identify successful permit applications part 2

---
+++ src/Command/Permits/QueueRunScoring.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Queue run scoring
+ *
+ * @author Jonathan Thomas <jonathan@opalise.co.uk>
+ */
+namespace Dvsa\Olcs\Transfer\Command\Permits;
+
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
+
+/**
+ * @Transfer\RouteName("backend/permits/queue-run-scoring")
+ * @Transfer\Method("POST")
+ */
+final class QueueRunScoring extends AbstractCommand
+{
+    use Identity;
+}
