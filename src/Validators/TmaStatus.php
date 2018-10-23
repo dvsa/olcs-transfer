3f77c98694ff69fc5076a8f8504edc97b352c478
Igor Stimoli <igor.stimoli@bjss.com>
2018-10-23 16:07:21 +0100

    OLCS-15057: TMA Submit now can receive status

---
+++ src/Validators/TmaStatus.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dvsa\Olcs\Transfer\Validators;
+use Common\Service\Entity\TransportManagerApplicationEntityService as Tma;
+
+class TmaStatus extends \Zend\Validator\InArray
+{
+    protected $haystack = [
+        'tmap_st_incomplete',
+        'tmap_st_awaiting_signature',
+        'tmap_st_tm_signed',
+        'tmap_st_operator_signed',
+        'tmap_st_postal_application',
+        'tmap_st_received',
+        'tmap_st_details_submitted',
+        'tmap_st_details_checked',
+        'tmap_st_operator_approved',
+    ];
+
+    protected $messageTemplates = array(
+        self::NOT_IN_ARRAY => 'Invalid status',
+    );
+}
