c899d99b6b2eedc0a84ddd60219008baea1f3b2c
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-11-21 13:53:07 +0000

    WIP OLCS-22272

--- src/Query/Surrender/ById.php
+++ src/Query/Surrender/ById.php
@@ -3,8 +3,17 @@
 
 namespace Dvsa\Olcs\Transfer\Query\Surrender;
 
+use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 
-class ById
+/**
+ * Class ById
+ *
+ * @package Dvsa\Olcs\Transfer\Query\Surrender
+ * @Transfer\RouteName("backend/licence/single/surrender")
+ * @Transfer\Method("GET")
+ */
+class ById extends AbstractQuery
 {
-
+    use Identity;
 }
