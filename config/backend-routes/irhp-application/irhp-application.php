47dc29e858587ed93f2febaa56359b97a1bf7031
Jonathan Thomas <jonathan@opalise.co.uk>
2019-01-08 16:52:07 +0000

    OLCS-22907 ss new bilateral permits number of permits per country page

--- config/backend-routes/irhp-application/irhp-application.php
+++ config/backend-routes/irhp-application/irhp-application.php
@@ -1,6 +1,8 @@
 <?php
 
+use Dvsa\Olcs\Transfer\Command;
 use Dvsa\Olcs\Transfer\Query;
+use Dvsa\Olcs\Transfer\Router\CommandConfig;
 use Dvsa\Olcs\Transfer\Router\QueryConfig;
 use Dvsa\Olcs\Transfer\Router\RouteConfig;
 
@@ -17,6 +19,16 @@ return [
                     'GET' => QueryConfig::getConfig(Query\IrhpApplication\ById::class),
                 ]
             ),
+            'update-multiple-no-of-permits' => [
+                'type' => 'Segment',
+                'options' => [
+                    'route' => 'update-multiple-no-of-permits[/]',
+                ],
+                'may_terminate' => false,
+                'child_routes' => [
+                    'PUT' => CommandConfig::getPutConfig(Command\IrhpApplication\UpdateMultipleNoOfPermits::class),
+                ],
+            ], 
         ]
     ],
 ];
