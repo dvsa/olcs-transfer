af72ad0dc5f843541371ce3b2665686b7bed2ed9
Ian Lindsay <ian@hemera-business-services.co.uk>
2019-01-09 10:11:45 +0000

    olcs-22991 system params can now be up to 255 characters

--- src/Command/SystemParameter/UpdateSystemParameter.php
+++ src/Command/SystemParameter/UpdateSystemParameter.php
@@ -22,7 +22,7 @@ final class UpdateSystemParameter extends AbstractCommand
 
     /**
      * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
-     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options":{"min":1, "max":32}})
+     * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options":{"min":1, "max":255}})
      */
     protected $paramValue;
 
