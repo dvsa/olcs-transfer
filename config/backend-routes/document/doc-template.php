05fb33ccf305acf9ab622d7b8fdfc34945cdb0d2
Andy Newton <andy@vitri.ltd>
2019-03-20 13:36:52 +0000

    OLCS-22341 - feedback tweaks

--- config/backend-routes/document/doc-template.php
+++ config/backend-routes/document/doc-template.php
@@ -7,10 +7,32 @@ use Dvsa\Olcs\Transfer\Router\QueryConfig;
 use Dvsa\Olcs\Transfer\Router\RouteConfig;
 
 return [
-    'doc-template' => RouteConfig::getRouteConfig(
-        'doc-template',
-        [
+    'doc-template' => [
+        'type' => 'Segment',
+        'options' => [
+            'route' => 'doc-template[/]',
+        ],
+        'may_terminate' => false,
+        'child_routes' => [
+            'single' => RouteConfig::getSingleConfig(
+                [
+                    'GET' => QueryConfig::getConfig(Query\DocTemplate\ById::class),
+                    'POST' => CommandConfig::getPostConfig(Command\DocTemplate\Update::class),
+                    'DELETE' => CommandConfig::getDeleteConfig(Command\DocTemplate\Delete::class),
+                ]
+            ),
             'GET' => QueryConfig::getConfig(Query\DocTemplate\GetList::class),
+            'POST' => CommandConfig::getPostConfig(Command\DocTemplate\Create::class),
+            'admin-list' => [
+                'type' => 'Segment',
+                'options' => [
+                    'route' => 'admin-list[/]',
+                ],
+                'may_terminate' => false,
+                'child_routes' => [
+                    'GET' => QueryConfig::getConfig(Query\DocTemplate\FullList::class),
+                ]
+            ],
         ]
-    )
+    ],
 ];
