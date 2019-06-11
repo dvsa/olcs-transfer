7a91f8c7fb491ee4e584a942c8be9f1b6fea45bd
Piotr Filak <piotr.filak@capgemini.com>
2019-06-11 15:16:32 +0100

    OLCS-24728 - Remove Emissions standard from Window for Annual ECMT 2019/20

--- test/src/Command/IrhpPermitStock/UpdateTest.php
+++ test/src/Command/IrhpPermitStock/UpdateTest.php
@@ -27,5 +27,27 @@ class UpdateTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($data['validTo'], $command->getValidTo());
         $this->assertEquals($data['irhpPermitType'], $command->getIrhpPermitType());
         $this->assertEquals($data['initialStock'], $command->getInitialStock());
+        $this->assertEquals('emissions_cat_na', $command->getEmissionsCategory());
+    }
+
+    public function testStructureWithEmissionsCategory()
+    {
+        $data = [
+            'id' => '2',
+            'validFrom' => '2029-01-01',
+            'validTo' => '2029-12-31',
+            'initialStock' => '1400',
+            'irhpPermitType' => '1',
+            'emissionsCategory' => 'emissions_cat_euro6'
+        ];
+
+        $command = Update::create($data);
+
+        $this->assertEquals($data['id'], $command->getId());
+        $this->assertEquals($data['validFrom'], $command->getValidFrom());
+        $this->assertEquals($data['validTo'], $command->getValidTo());
+        $this->assertEquals($data['irhpPermitType'], $command->getIrhpPermitType());
+        $this->assertEquals($data['initialStock'], $command->getInitialStock());
+        $this->assertEquals('emissions_cat_euro6', $command->getEmissionsCategory());
     }
 }
