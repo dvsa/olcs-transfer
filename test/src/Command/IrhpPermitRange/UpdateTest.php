eff5134590995c6b06b377c41660eda99e8378db
Piotr Filak <piotr.filak@capgemini.com>
2019-06-14 15:15:08 +0100

    OLCS-24729 - Admin user selects an emissions standard for an Annual ECMT stock range - for use in ECMT Annual 2019/20

---
+++ test/src/Command/IrhpPermitRange/UpdateTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Dvsa\OlcsTest\Transfer\Command\IrhpPermitRange;
+
+use Dvsa\Olcs\Transfer\Command\IrhpPermitRange\Update;
+
+/**
+ * Update test
+ */
+class UpdateTest extends \PHPUnit\Framework\TestCase
+{
+    public function testStructure()
+    {
+        $data = [
+            'id' => 100,
+            'irhpPermitStock' => '1',
+            'prefix' => 'UK',
+            'fromNo' => 1,
+            'toNo' => 2,
+            'lostReplacement' => 1,
+            'ssReserve' => 1,
+            'countrys' => ['DE'],
+        ];
+
+        $command = Update::create($data);
+
+        $this->assertEquals($data['id'], $command->getId());
+        $this->assertEquals($data['irhpPermitStock'], $command->getIrhpPermitStock());
+        $this->assertEquals('emissions_cat_na', $command->getEmissionsCategory());
+        $this->assertEquals($data['prefix'], $command->getPrefix());
+        $this->assertEquals($data['fromNo'], $command->getFromNo());
+        $this->assertEquals($data['toNo'], $command->getToNo());
+        $this->assertEquals($data['lostReplacement'], $command->getIsLostReplacement());
+        $this->assertEquals($data['ssReserve'], $command->getSsReserve());
+        $this->assertEquals($data['countrys'], $command->getRestrictedCountries());
+    }
+
+    public function testStructureWithEmissionsCategory()
+    {
+        $data = [
+            'id' => 100,
+            'irhpPermitStock' => '1',
+            'prefix' => 'UK',
+            'fromNo' => 1,
+            'toNo' => 2,
+            'lostReplacement' => 1,
+            'ssReserve' => 1,
+            'countrys' => ['DE'],
+            'emissionsCategory' => 'emissions_cat_euro6'
+        ];
+
+        $command = Update::create($data);
+
+        $this->assertEquals($data['id'], $command->getId());
+        $this->assertEquals($data['irhpPermitStock'], $command->getIrhpPermitStock());
+        $this->assertEquals('emissions_cat_euro6', $command->getEmissionsCategory());
+        $this->assertEquals($data['prefix'], $command->getPrefix());
+        $this->assertEquals($data['fromNo'], $command->getFromNo());
+        $this->assertEquals($data['toNo'], $command->getToNo());
+        $this->assertEquals($data['lostReplacement'], $command->getIsLostReplacement());
+        $this->assertEquals($data['ssReserve'], $command->getSsReserve());
+        $this->assertEquals($data['countrys'], $command->getRestrictedCountries());
+    }
+}
