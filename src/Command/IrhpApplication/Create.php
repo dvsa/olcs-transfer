6dd1ff1dae30b5235bb6193754fba77bdf03b789
Ian Lindsay <ian@hemera-business-services.co.uk>
2019-10-02 21:17:41 +0100

    olcs-25615 alter permit application create handler for short term 2020

--- src/Command/IrhpApplication/Create.php
+++ src/Command/IrhpApplication/Create.php
@@ -5,8 +5,9 @@
  */
 namespace Dvsa\Olcs\Transfer\Command\IrhpApplication;
 
+use Dvsa\Olcs\Transfer\FieldType\Traits\IrhpPermitStockOptional;
+use Dvsa\Olcs\Transfer\FieldType\Traits\IrhpPermitType;
 use Dvsa\Olcs\Transfer\FieldType\Traits\Licence;
-use Dvsa\Olcs\Transfer\FieldType\Traits\YearOptional;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\Command\AbstractCommand;
 
@@ -17,26 +18,6 @@ use Dvsa\Olcs\Transfer\Command\AbstractCommand;
 final class Create extends AbstractCommand
 {
     use Licence;
-    use YearOptional;
-
-    /**
-     * @var string
-     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
-     * @Transfer\Validator({
-     *      "name":"Zend\Validator\Between",
-     *      "options": {
-     *          "min": 0,
-     *          "max": 999999
-     *      }
-     * })
-     */
-    protected $type;
-
-    /**
-     * @return int
-     */
-    public function getType(): int
-    {
-        return $this->type;
-    }
+    use IrhpPermitType;
+    use IrhpPermitStockOptional;
 }
