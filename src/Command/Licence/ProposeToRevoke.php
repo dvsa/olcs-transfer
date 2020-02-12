5fe4794ed25c5cea1e05c1cae3161ae519f2fe67
James Hodgson <james.hodgson@dvsa.gov.uk>
2020-02-12 09:02:12 +0000

    Added document id to command

--- src/Command/Licence/ProposeToRevoke.php
+++ src/Command/Licence/ProposeToRevoke.php
@@ -13,4 +13,19 @@ use Dvsa\Olcs\Transfer\Command\AbstractCommand;
 class ProposeToRevoke extends AbstractCommand
 {
     use Licence;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $document;
+
+    /**
+     * @return mixed
+     */
+    public function getDocument()
+    {
+        return $this->document;
+    }
 }
