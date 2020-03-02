2b8453e212b6810eb67a0fc87f6d57668f1a0ce1
Parth <parth.vyas@dvsa.gov.uk>
2020-03-02 10:47:39 +0000

    VOL-1-letter-fix: Create CreateLetterTest.php.

---
+++ test/src/Command/Document/CreateLetterTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Dvsa\OlcsTest\Transfer\Command\Document;
+
+use PHPUnit\Framework\TestCase;
+use \Dvsa\Olcs\Transfer\Command\Document\CreateLetter;
+use Dvsa\OlcsTest\Transfer\Command\CommandTest;
+
+/**
+ * @covers CreateLetter
+ */
+class CreateLetterTest extends TestCase
+{
+    use CommandTest;
+
+    /**
+     * @inheritDoc
+     */
+    protected function createBlankDto()
+    {
+        return new CreateLetter();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getOptionalDtoFields()
+    {
+        return [
+            'disableBookmarks',
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getValidFieldValues()
+    {
+        return [
+            'template' => [
+                '1',
+                '1001',
+            ],
+            'data' => [
+                'some-string',
+                '191',
+                ['an-array'],
+                '10.1'
+            ],
+            'disableBookmarks' => [
+                'Y',
+                'N'
+            ],
+            'meta' => [
+                'a-string',
+                21098,
+                ['some-array'],
+                90.1
+            ]
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getInvalidFieldValues()
+    {
+        return [
+            'template' => [
+                ['array' => 'stuff'],
+                []
+            ],
+            'disableBookmarks' => [
+                'Hello',
+                ['array'],
+                123,
+                '123'
+            ],
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getFilterTransformations()
+    {
+        return [
+            'template' => [
+                [1, '1'],
+                ['2 ', '2']
+            ],
+            'disableBookmarks' => [
+                ['Y ', 'Y'],
+                ['N ', 'N']
+            ]
+        ];
+    }
+}
