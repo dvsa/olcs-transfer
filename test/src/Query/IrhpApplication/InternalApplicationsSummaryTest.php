c17f6bf6c23351ab68dc0dbd6ef13e0ef572e782
Piotr Filak <piotr.filak@capgemini.com>
2020-05-21 15:32:10 +0100

    OLCS-27025 - Update to IRHP permits page

--- test/src/Query/IrhpApplication/InternalApplicationsSummaryTest.php
+++ test/src/Query/IrhpApplication/InternalApplicationsSummaryTest.php
@@ -14,13 +14,34 @@ class InternalApplicationsSummaryTest extends \PHPUnit\Framework\TestCase
     {
         $sut = InternalApplicationsSummary::create(
             [
-              'licence' => 14,
+                'licence' => 14,
             ]
         );
         $this->assertEquals(14, $sut->getLicence());
+        $this->assertNull($sut->getStatus());
         $this->assertEquals(
             [
                 'licence' => 14,
+                'status' => null,
+            ],
+            $sut->getArrayCopy()
+        );
+    }
+
+    public function testStructureOptional()
+    {
+        $sut = InternalApplicationsSummary::create(
+            [
+                'licence' => 14,
+                'status' => 'app_status',
+            ]
+        );
+        $this->assertEquals(14, $sut->getLicence());
+        $this->assertEquals('app_status', $sut->getStatus());
+        $this->assertEquals(
+            [
+                'licence' => 14,
+                'status' => 'app_status',
             ],
             $sut->getArrayCopy()
         );
