00a85871421997a1f279720c690dd3422ae9fd60
James Wragg <james.wragg@dvsa.gov.uk>
2020-05-29 15:59:25 +0100

    Unit tests

---
+++ test/src/Command/User/UpdateUserLastLoginAtTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Dvsa\OlcsTest\Transfer\Command\User;
+
+use Dvsa\Olcs\Transfer\Command\User\UpdateUserLastLoginAt;
+use Dvsa\OlcsTest\Transfer\Command\CommandTest;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Pay Outstanding Fees test
+ */
+class UpdateUserLastLoginAtTest extends TestCase
+{
+    use CommandTest;
+
+    protected function createBlankDto()
+    {
+        return new UpdateUserLastLoginAt();
+    }
+
+    protected function getOptionalDtoFields()
+    {
+        return [];
+    }
+
+    protected function getValidFieldValues()
+    {
+        return [
+            'secureToken' => [
+                'AQIC5wM2LY4SfczoZQTpr-5U3AuSvh7z3PWQQVMBF7ws3t0.%2AAAJTSQACMDIAAlNLABM4Nzk1ODA5NTg5MjA2NDMwNjk2AAJTMQACMDE.%2A',
+                'AQIC5wM2LY4Sfcx-A3rZVxSIGZoaqwJjloBVJEHl2ThcxOc.%2AAAJTSQACMDIAAlNLABQtNDU1NzIzMzk3MDI5NTM1MjkwNAACUzEAAjAx%2A'
+            ]
+        ];
+    }
+
+    protected function getInvalidFieldValues()
+    {
+        return [];
+    }
+
+
+    protected function getFilterTransformations()
+    {
+        return [
+            'secureToken' => [
+                [
+                    '   AQIC5wM2LY4SfczoZQTpr-5U3AuSvh7z3PWQQVMBF7ws3t0.%2AAAJTSQACMDIAAlNLABM4Nzk1ODA5NTg5MjA2NDMwNjk2AAJTMQACMDE.%2A',
+                    'AQIC5wM2LY4SfczoZQTpr-5U3AuSvh7z3PWQQVMBF7ws3t0.%2AAAJTSQACMDIAAlNLABM4Nzk1ODA5NTg5MjA2NDMwNjk2AAJTMQACMDE.%2A'
+                ],
+                [
+                    'AQIC5wM2LY4Sfcx-A3rZVxSIGZoaqwJjloBVJEHl2ThcxOc.%2AAAJTSQACMDIAAlNLABQtNDU1NzIzMzk3MDI5NTM1MjkwNAACUzEAAjAx%2A    ',
+                    'AQIC5wM2LY4Sfcx-A3rZVxSIGZoaqwJjloBVJEHl2ThcxOc.%2AAAJTSQACMDIAAlNLABQtNDU1NzIzMzk3MDI5NTM1MjkwNAACUzEAAjAx%2A',
+                ]
+            ]
+        ];
+    }
+}
