0202df0ad7cdcbb2598b63f045027c2d7aa2b25c
Andy Newton <andy@vitri.ltd>
2020-08-04 16:53:17 +0100

    OLCS-26627

--- src/FieldType/Traits/TranslationSearchOptional.php
+++ src/FieldType/Traits/TranslationSearchOptional.php
@@ -2,12 +2,12 @@
 namespace Dvsa\Olcs\Transfer\FieldType\Traits;
 
 /**
- * Translated Text Optional
+ * Translation Key Search Optional
  *
  * @package Dvsa\Olcs\Transfer\Command\Traits\FieldType
  * @author Andy Newton <andy@vitri.ltd>
  */
-trait TranslatedTextOptional
+trait TranslationSearchOptional
 {
     /**
      * @var String
@@ -15,13 +15,13 @@ trait TranslatedTextOptional
      * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
      * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options":{"max":1024}})
      */
-    protected $translatedText;
+    protected $translationSearch;
 
     /**
      * @return string
      */
-    public function getTranslatedText()
+    public function getTranslationSearch()
     {
-        return $this->translatedText;
+        return $this->translationSearch;
     }
 }
