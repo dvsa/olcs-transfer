00580e643e4b6c296cf4b9b40f15ba0302bf031e
James Wragg <james.wragg@dvsa.gov.uk>
2020-08-21 14:52:04 +0100

    Fixed route and filter bug

--- src/Query/DvlaSearch/Vehicle.php
+++ src/Query/DvlaSearch/Vehicle.php
@@ -7,13 +7,13 @@ use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 
 /**
- * @Transfer\RouteName("backend/dvla-seach/vehicle")
+ * @Transfer\RouteName("backend/dvla-search/vehicle")
  */
 class Vehicle extends AbstractQuery
 {
     /**
      * @var string
-     * @Transfer\Filter({"name":"Zend\Filter\String"})
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
      * @Transfer\Validator({"name":"Zend\Validator\StringLength", "options": {"min": 3, "max":10}})
      */
     protected $vrn;
