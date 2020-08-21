1fd873077b65cd32039e2225f93555baa869ce84
James Wragg <james.wragg@dvsa.gov.uk>
2020-08-21 16:03:49 +0100

    Added UnitTest for DvlaSearch/Vehicle Query

---
+++ test/src/Query/DvlaSearch/VehicleTest.php
@@ -0,0 +1,141 @@
+<?php declare(strict_types=1);
+
+
+namespace Dvsa\OlcsTest\Transfer\Query\DvlaSearch;
+
+use Dvsa\Olcs\Transfer\Query\DvlaSearch\Vehicle;
+use Dvsa\OlcsTest\Transfer\Query\QueryTest;
+use PHPUnit\Framework\TestCase;
+use Zend\Stdlib\ArraySerializableInterface;
+
+class VehicleTest extends TestCase
+{
+    use QueryTest;
+
+
+    /**
+     * Should return a new blank DTO on which to run tests
+     *
+     * @return ArraySerializableInterface
+     */
+    protected function createBlankDto()
+    {
+        return new Vehicle();
+    }
+
+    /**
+     * Should return a list of optional fields
+     *
+     * for example:
+     *
+     * return ['optionalField', 'anotherOptionalField']
+     *
+     * Each field is expected to be set to null after validation
+     *
+     * @return string[]
+     */
+    protected function getOptionalDtoFields()
+    {
+        return [];
+    }
+
+    /**
+     * Should return an array of valid field values (i.e. those which should pass validation)
+     *
+     * for example:
+     *
+     * return [
+     *     'fieldName' => [
+     *         'good-value-1',
+     *         'good-value-2',
+     *     ],
+     *     'anotherFieldName' => ['good-value'],
+     * ];
+     *
+     * @return array
+     */
+    protected function getValidFieldValues()
+    {
+        return [
+            'vrn' => [
+                'A12',
+                'A123',
+                'A1234',
+                'A12345',
+                'ABC1234',
+                'A12039FJ',
+                'A12039FJ9',
+                'A12039FJ90',
+            ]
+        ];
+    }
+
+    /**
+     * Should return an array of invalid field values (i.e. those which should fail validation)
+     *
+     * for example:
+     *
+     * return [
+     *     'fieldName' => [
+     *         'bad-value-1',
+     *         'bad-value-2',
+     *     ],
+     *     'anotherFieldName' => ['bad-value'],
+     * ];
+     *
+     * @return array
+     */
+    protected function getInvalidFieldValues()
+    {
+        return [
+            'vrn' => [
+                "AA",
+                "12345678901",
+                "ABCD!!!",
+                "ABCD!!!",
+                "ABCD!!!",
+                "ABC1234!",
+                "x",
+                [],
+                null,
+                true,
+                false,
+            ]
+        ];
+    }
+
+    /**
+     * Should return an array of expected transformations which input filters should apply to fields
+     *
+     * for example:
+     *
+     * return [
+     *     'fieldWhichGetsTrimmed' => [[' string ', 'string']],
+     *     'fieldWhichFiltersOutNonNumericDigits => [
+     *         ['a1b2c3', '123'],
+     *         [99, '99'],
+     *     ],
+     * ];
+     *
+     * Tests expect the function 'getFoo' to exist for the function 'foo'.
+     *
+     * This DOES NOT assert that the value gets validated. To do that @see DtoTest::getValidFieldValues
+     *
+     * @return array
+     */
+    protected function getFilterTransformations()
+    {
+        return [
+            'vrn' => [
+                [' ABC123', 'ABC123'],
+                [' ABC123 ', 'ABC123'],
+                ['ABC123 ', 'ABC123'],
+                ['AB12 3DE', 'AB123DE'],
+                ['AB 1 2 3DE', 'AB123DE'],
+                ['AB12	3DE', 'AB123DE'],
+                ['  A B 1 2	 3
+                D E  ', 'AB123DE'],
+            ],
+        ];
+    }
+}
