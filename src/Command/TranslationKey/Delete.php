223d9f13c8391598496c681247140ef38f49d8ea
Andy Newton <andy@vitri.ltd>
2020-11-05 13:11:14 +0000

    add and delete keys

---
+++ src/Command/TranslationKey/Delete.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Delete Translation Key
+ */
+namespace Dvsa\Olcs\Transfer\Command\TranslationKey;
+
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+use Dvsa\Olcs\Transfer\FieldType\Traits\IdentityString;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+
+/**
+ * @Transfer\RouteName("backend/translation-key/single")
+ * @Transfer\Method("DELETE")
+ */
+final class Delete extends AbstractCommand
+{
+    use IdentityString;
+}
