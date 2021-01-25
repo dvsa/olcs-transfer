ea2e7c1e46a48e2307d66373e35bef992f6440b8
Marcus Turner <marcus.turner@dvsa.gov.uk>
2021-01-25 15:37:05 +0000

    VOL-132: HGV - Create 'Vehicle details' page

---
+++ test/src/Query/Licence/GoodsVehiclesTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Dvsa\OlcsTest\Transfer\Query\Licence;
+
+use Dvsa\Olcs\Transfer\Query\Licence\GoodsVehicles;
+use Dvsa\Olcs\Transfer\Query\QueryContainer;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+use Dvsa\Olcs\Transfer\Util\Annotation\AnnotationBuilder;
+use PHPUnit\Framework\TestCase;
+
+class GoodsVehiclesTest extends TestCase
+{
+    public function testGetVehicleIdsIsDefined(): void
+    {
+        // Setup
+        $sut = GoodsVehicles::create([]);
+
+        // Assert
+        $this->assertIsCallable([$sut, 'getVehicleIds']);
+    }
+
+    /**
+     * @depends testGetVehicleIdsIsDefined
+     */
+    public function testGetVehicleIdsReturnsAnArrayProvided()
+    {
+        // Setup
+        $expectedVehicleIds = [1, 2, 3, 4];
+        $sut = GoodsVehicles::create(['vehicleIds' => $expectedVehicleIds]);
+
+        // Assert
+        $this->assertEquals($expectedVehicleIds, $sut->getVehicleIds());
+    }
+
+    /**
+     * @depends testGetVehicleIdsIsDefined
+     */
+    public function testGetVehicleIdsReturnsNullProvided()
+    {
+        // Setup
+        $sut = GoodsVehicles::create(['vehicleIds' => null]);
+
+        // Assert
+        $this->assertNull($sut->getVehicleIds());
+    }
+
+    /**
+     * @depends testGetVehicleIdsIsDefined
+     */
+    public function testGetVehicleIdsReturnsNullWhenNoVehicleIdsProvided()
+    {
+        // Setup
+        $sut = GoodsVehicles::create([]);
+
+        // Assert
+        $this->assertNull($sut->getVehicleIds());
+    }
+
+    /**
+     * @return array[]
+     */
+    public function getValidVehicleIdSets(): array
+    {
+        return [
+            'no vehicle ids' => [],
+            'empty array of vehicle ids' => [[]],
+            'max number of vehicle ids' => [array_fill(0, 100, 1)],
+            'numeric string vehicle id' => [["1"]],
+            'associative vehicle id array' => [["foo" => 1]],
+        ];
+    }
+
+    /**
+     * @dataProvider getValidVehicleIdSets
+     */
+    public function testItAcceptsValidSetsOfVehicleIds(array $vehicleIdsSet = null): void
+    {
+        // Setup
+        $sut = GoodsVehicles::create(['vehicleIds' => $vehicleIdsSet]);
+        $queryContainer = $this->newQueryContainer($sut);
+
+        // Execute
+        $queryContainer->isValid();
+
+        // Assert
+        $this->assertArrayNotHasKey('vehicleIds', $queryContainer->getMessages(), 'Expected query to have no validation messages for the "vehiclesIds" property');
+    }
+
+    /**
+     * @return array[]
+     */
+    public function getInvalidVehicleIdSets(): array
+    {
+        return [
+            'too many vehicle ids' => [array_fill(0, 101, 1)],
+            'zero vehicle id' => [[0]],
+            'negative vehicle id' => [[-1]],
+            'decimal vehicle id' => [[1.05]],
+            'alpha vehicle id' => [["a"]],
+        ];
+    }
+
+    /**
+     * @dataProvider getInvalidVehicleIdSets
+     */
+    public function testItDoesNotAcceptInvalidSetsOfVehicleIds(array $vehicleIdsSet = null): void
+    {
+        // Setup
+        $sut = GoodsVehicles::create(['vehicleIds' => $vehicleIdsSet]);
+        $queryContainer = $this->newQueryContainer($sut);
+
+        // Execute
+        $queryContainer->isValid();
+
+        // Assert
+        $this->assertArrayHasKey('vehicleIds', $queryContainer->getMessages(), 'Expected query to have validation messages for the "vehiclesIds" property');
+    }
+
+    /**
+     * @param QueryInterface $query
+     * @return QueryContainer
+     */
+    public function newQueryContainer(QueryInterface $query): QueryContainer
+    {
+        return (new AnnotationBuilder())->createQuery($query);
+    }
+}
