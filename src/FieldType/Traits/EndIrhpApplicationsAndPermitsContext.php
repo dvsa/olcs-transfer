b8291b1404c9b0fa765a321df222de0a9973fb66
Jonathan Thomas <jonathan@opalise.co.uk>
2021-02-17 10:47:26 +0000

    OLCS-28239 create a task when surrendering revoking or cns a licence when there are permits or permit applications

---
+++ src/FieldType/Traits/EndIrhpApplicationsAndPermitsContext.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Dvsa\Olcs\Transfer\FieldType\Traits;
+
+/**
+ * End IRHP Applications and Permits Context Trait
+ *
+ * @package Dvsa\Olcs\Transfer\FieldType\Traits
+ * @author Jonathan Thomas <jonathan@opalise.co.uk>
+ */
+trait EndIrhpApplicationsAndPermitsContext
+{
+    /**
+     * @Transfer\Filter({"name":"Laminas\Filter\StringTrim"})
+     * @Transfer\Validator({"name":"Laminas\Validator\StringLength", "options":{"min": 1}})
+     * @Transfer\Validator({
+     *     "name":"Laminas\Validator\InArray",
+     *     "options": {
+     *         "haystack": {"context_surrender","context_revoke","context_cns"}
+     *     }
+     * })
+     */
+    protected $context;
+
+    /**
+     * @return string
+     */
+    public function getContext()
+    {
+        return $this->context;
+    }
+}
