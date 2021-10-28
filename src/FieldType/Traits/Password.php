f81e88a731afb009a5bf7c476104ab2ff150e183
James Hodgson <james.hodgson@dvsa.gov.uk>
2021-10-28 15:52:33 +0000

    VOL-2159: Don't escape passwords

--- src/FieldType/Traits/Password.php
+++ src/FieldType/Traits/Password.php
@@ -6,6 +6,7 @@ trait Password
     /**
      * @var String
      * @Transfer\Validator({"name":"Laminas\Validator\StringLength", "options":{"min":8, "max":255}})
+     * @Transfer\Escape(false)
      */
     protected $password = null;
 
