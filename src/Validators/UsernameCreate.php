92f3c75d88c955e9f419ddfb11c828b8fcec8534
James Wragg <james.wragg@dvsa.gov.uk>
2022-05-17 14:55:27 +0000

    VOL-3110: Created UsernameCreate validator to enforce strict username rules when creating new users

---
+++ src/Validators/UsernameCreate.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Dvsa\Olcs\Transfer\Validators;
+
+use Laminas\Validator\StringLength;
+
+/**
+ * Ensure the username matches the newly defined required criteria.
+ */
+class UsernameCreate extends StringLength
+{
+    const USERNAME_LENGTH_MIN = 4;
+    const USERNAME_LENGTH_MAX = 40;
+    const USERNAME_INVALID = 'usernameCreateInvalid';
+
+    /**
+     * Sets validator options
+     *
+     * @param  array
+     */
+    public function __construct($options = array())
+    {
+        $options['min'] = self::USERNAME_LENGTH_MIN;
+        $options['max'] = self::USERNAME_LENGTH_MAX;
+
+        $this->messageTemplates[self::USERNAME_INVALID] = 'error.form-validator.usernameCreate.invalid';
+
+        parent::__construct($options);
+    }
+
+    /**
+     * Check if username is valid
+     *
+     * @param string $value
+     * @return bool
+     */
+    public function isValid($value)
+    {
+        if (parent::isValid($value)) {
+            if (preg_match('/^[a-z][a-z0-9]{3,39}$/', $value)) {
+                return true;
+            }
+
+            $this->error(self::USERNAME_INVALID);
+        }
+
+        return false;
+    }
+}
