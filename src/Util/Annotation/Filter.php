99f67d1def45d082bf2f95fd90f2a688ccc80577
Saul Wilcox <saul.wilcox@dvsa.gov.uk>
2023-04-21 17:26:04 +0100

    Update Validator Annotation

--- src/Util/Annotation/Filter.php
+++ src/Util/Annotation/Filter.php
@@ -7,28 +7,88 @@
  */
 namespace Dvsa\Olcs\Transfer\Util\Annotation;
 
-use Laminas\Form\Annotation\Filter as LaminasFilter;
-
 /**
  * @Annotation
  */
-class Filter extends LaminasFilter
-{
-    public function getName()
-    {
-        $spec = $this->getFilter();
 
-        return $spec['name'];
+class Filter
+{
+    /** @var string */
+    protected $name;
+
+    /** @var array */
+    protected $options;
+
+    /** @var int|null */
+    protected $priority;
+
+    /**
+     * Receive and process the contents of an annotation
+     *
+     * @param string|array $name
+     * @param array $options
+     */
+    public function __construct($name, array $options = [], ?int $priority = null)
+    {
+        if (is_array($name)) {
+            // support for legacy notation with array as first parameter
+            trigger_error(sprintf(
+                'Passing a single array to the constructor of %s is deprecated since 3.0.0,'
+                . ' please use separate parameters.',
+                static::class
+            ), E_USER_DEPRECATED);
+
+            $this->name     = $name['name'] ?? null;
+            $this->options  = $name['options'] ?? $options;
+            $this->priority = $name['priority'] ?? $priority;
+        } else {
+            $this->name     = $name;
+            $this->options  = $options;
+            $this->priority = $priority;
+        }
     }
 
-    public function getOptions()
+    /**
+     * Retrieve the filter specification
+     *
+     * @return array
+     */
+    public function getFilterSpecification(): array
     {
-        $spec = $this->getFilter();
-
-        if (empty($spec['options'])) {
-            return null;
+        $inputSpec = ['name' => $this->name];
+        if (! empty($this->options)) {
+            $inputSpec['options'] = $this->options;
+        }
+        if (null !== $this->priority) {
+            $inputSpec['priority'] = $this->priority;
         }
 
-        return $spec['options'];
+        return $inputSpec;
     }
+
+    /**
+     * @return mixed|string|null
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return array|mixed
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @return int|mixed|null
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
 }
+
--- src/Util/Annotation/Validator.php
+++ src/Util/Annotation/Validator.php
@@ -7,13 +7,36 @@
  */
 namespace Dvsa\Olcs\Transfer\Util\Annotation;
 
-use Laminas\Form\Annotation\Validator as LaminasValidator;
+use Laminas\Form\Exception;
 
 /**
  * @Annotation
  */
-class Validator extends LaminasValidator
+class Validator
 {
+    /**
+     * @var array
+     */
+    protected $value;
+
+    /**
+     * Receive and process the contents of an annotation
+     *
+     * @param  array $data
+     * @throws Exception\DomainException if a 'value' key is missing, or its value is not an array
+     */
+    public function __construct(array $data)
+    {
+        if (! isset($data['value']) || ! is_array($data['value'])) {
+            throw new Exception\DomainException(sprintf(
+                '%s expects the annotation to define an array; received "%s"',
+                get_class($this),
+                isset($data['value']) ? gettype($data['value']) : 'null'
+            ));
+        }
+        $this->value = $data['value'];
+    }
+
     public function getName()
     {
         $spec = $this->getValidator();
@@ -30,5 +53,15 @@ class Validator extends LaminasValidator
         }
 
         return $spec['options'];
+    }
+
+    /**
+     * Retrieve the validator specification
+     *
+     * @return null|array
+     */
+    public function getValidator()
+    {
+        return $this->value;
     }
 }
