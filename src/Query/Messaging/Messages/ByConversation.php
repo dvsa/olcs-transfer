800c9c735a9493d45a5b75ad5a41bee2230399d7
Saul Wilcox <saul.wilcox@dvsa.gov.uk>
2023-12-13 12:59:51 +0000

    Add Test

--- src/Query/Messaging/Messages/ByConversation.php
+++ src/Query/Messaging/Messages/ByConversation.php
@@ -2,12 +2,11 @@
 
 namespace Dvsa\Olcs\Transfer\Query\Messaging\Messages;
 
-use Dvsa\Olcs\Transfer\Query\OrderedQueryInterface;
+use Dvsa\Olcs\Transfer\FieldType\Traits\Conversation;
 use Dvsa\Olcs\Transfer\Query\PagedQueryInterface;
 use Dvsa\Olcs\Transfer\Query\PagedTrait;
-use Dvsa\Olcs\Transfer\FieldType\Traits\Licence;
-use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 
 /**
  * @Transfer\RouteName("backend/messaging/messages/by-conversation")
@@ -15,22 +14,5 @@ use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 final class ByConversation extends AbstractQuery implements PagedQueryInterface
 {
     use PagedTrait;
-
-    /**
-     * @var int
-     * @Transfer\Filter({"name":"Laminas\Filter\Digits"})
-     * @Transfer\Validator({"name":"Laminas\Validator\Digits"})
-     * @Transfer\Validator({"name":"Laminas\Validator\GreaterThan", "options": {"min": 0}})
-     */
-    protected $conversation;
-
-    /**
-     * Get conversation
-     *
-     * @return int
-     */
-    public function getConversation()
-    {
-        return $this->conversation;
-    }
+    use Conversation;
 }
