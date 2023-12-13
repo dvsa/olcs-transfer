be4d76df9472fc1a3d075c5953184fdca8400b35
Saul Wilcox <saul.wilcox@dvsa.gov.uk>
2023-12-13 12:58:54 +0000

    Revert "Test and annotation fixes"
    This reverts commit 0d4627ef27bd47c55927b877e4dbce8aef6879cb.

--- src/FieldType/Traits/Conversation.php
+++ src/FieldType/Traits/Conversation.php
@@ -11,9 +11,9 @@ trait Conversation
 {
     /**
      * @var int
-     * @Transfer\Filter("Laminas\Filter\Digits")
-     * @Transfer\Validator("Laminas\Validator\Digits")
-     * @Transfer\Validator("Laminas\Validator\GreaterThan", options={"min": 0})
+     * @Transfer\Filter({"name":"Laminas\Filter\Digits"})
+     * @Transfer\Validator({"name":"Laminas\Validator\Digits"})
+     * @Transfer\Validator({"name":"Laminas\Validator\GreaterThan", "options": {"min": 0}})
      */
     protected $conversation;
 
--- src/Query/Messaging/Messages/ByConversation.php
+++ src/Query/Messaging/Messages/ByConversation.php
@@ -2,11 +2,12 @@
 
 namespace Dvsa\Olcs\Transfer\Query\Messaging\Messages;
 
-use Dvsa\Olcs\Transfer\FieldType\Traits\Conversation;
+use Dvsa\Olcs\Transfer\Query\OrderedQueryInterface;
 use Dvsa\Olcs\Transfer\Query\PagedQueryInterface;
 use Dvsa\Olcs\Transfer\Query\PagedTrait;
-use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\FieldType\Traits\Licence;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 
 /**
  * @Transfer\RouteName("backend/messaging/messages/by-conversation")
@@ -14,5 +15,22 @@ use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 final class ByConversation extends AbstractQuery implements PagedQueryInterface
 {
     use PagedTrait;
-    use Conversation;
+
+    /**
+     * @var int
+     * @Transfer\Filter({"name":"Laminas\Filter\Digits"})
+     * @Transfer\Validator({"name":"Laminas\Validator\Digits"})
+     * @Transfer\Validator({"name":"Laminas\Validator\GreaterThan", "options": {"min": 0}})
+     */
+    protected $conversation;
+
+    /**
+     * Get conversation
+     *
+     * @return int
+     */
+    public function getConversation()
+    {
+        return $this->conversation;
+    }
 }
