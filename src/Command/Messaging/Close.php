da7db8f9f4df030fb6b72e7df565ecffdf4809e5
wadedvsa <155439365+wadedvsa@users.noreply.github.com>
2024-01-08 16:07:35 +0000

    fix: static analyser

--- src/Command/Messaging/Close.php
+++ src/Command/Messaging/Close.php
@@ -2,15 +2,10 @@
 
 declare(strict_types=1);
 
-/**
- * Close conversation
- */
-
 namespace Dvsa\Olcs\Transfer\Command\Messaging;
 
 use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
-use Dvsa\Olcs\Transfer\FieldType\Traits\Conversation;
 use Dvsa\Olcs\Transfer\Command\AbstractCommand;
 
 /**
