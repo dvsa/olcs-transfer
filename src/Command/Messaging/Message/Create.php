cca22bdf2279c001db97c865c8e07c373849eda8
James Wragg <james.wragg@dvsa.gov.uk>
2024-01-15 14:39:26 +0000

    fix: Added Transfer import

--- src/Command/Messaging/Message/Create.php
+++ src/Command/Messaging/Message/Create.php
@@ -5,6 +5,7 @@ namespace Dvsa\Olcs\Transfer\Command\Messaging\Message;
 use Dvsa\Olcs\Transfer\Command\AbstractCommand;
 use Dvsa\Olcs\Transfer\FieldType\Traits\Messaging\Conversation;
 use Dvsa\Olcs\Transfer\FieldType\Traits\Messaging\MessageContent;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 
 /**
  * @Transfer\RouteName("backend/messaging/messages")
