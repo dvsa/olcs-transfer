c855f63743538f04876abeba0d16c9ee86c3dc88
Saul Wilcox <saul.wilcox@dvsa.gov.uk>
2024-01-24 13:58:17 +0000

    Import

--- src/Query/Messaging/Subjects/All.php
+++ src/Query/Messaging/Subjects/All.php
@@ -4,6 +4,8 @@ namespace Dvsa\Olcs\Transfer\Query\Messaging\Subjects;
 
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 use Dvsa\Olcs\Transfer\Query\CacheableLongTermQueryInterface;
+use Dvsa\Olcs\Transfer\Query\OrderedQueryInterface;
+use Dvsa\Olcs\Transfer\Query\OrderedTrait;
 use Dvsa\Olcs\Transfer\Query\PublicQueryCacheInterface;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 
@@ -11,9 +13,9 @@ use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
  * @Transfer\RouteName("backend/messaging/subjects/all")
  */
 class All extends AbstractQuery implements
-    \Dvsa\Olcs\Transfer\Query\OrderedQueryInterface,
+    OrderedQueryInterface,
     CacheableLongTermQueryInterface,
     PublicQueryCacheInterface
 {
-    use \Dvsa\Olcs\Transfer\Query\OrderedTrait;
+    use OrderedTrait;
 }
