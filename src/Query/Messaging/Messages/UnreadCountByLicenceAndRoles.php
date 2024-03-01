fc548e24e398b9182672c2a7b3a5181a41d7cf5c
James Wragg <5068769+jerotire@users.noreply.github.com>
2024-03-01 15:53:31 +0000

    feat: Updated UnreadCountByLicenceAndRoles (#48)

--- src/Query/Messaging/Messages/UnreadCountByLicenceAndRoles.php
+++ src/Query/Messaging/Messages/UnreadCountByLicenceAndRoles.php
@@ -3,15 +3,16 @@
 namespace Dvsa\Olcs\Transfer\Query\Messaging\Messages;
 
 use Dvsa\Olcs\Transfer\FieldType\Traits\Licence;
+use Dvsa\Olcs\Transfer\FieldType\Traits\RolesOptional;
 use Dvsa\Olcs\Transfer\FieldType\Traits\User;
 use Dvsa\Olcs\Transfer\Query\AbstractQuery;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 
 /**
- * @Transfer\RouteName("backend/messaging/messages/unread-count-by-licence-and-user")
+ * @Transfer\RouteName("backend/messaging/messages/unread-count-by-licence-and-roles")
  */
-final class UnreadCountByLicenceAndUser extends AbstractQuery
+final class UnreadCountByLicenceAndRoles extends AbstractQuery
 {
     use Licence;
-    use User;
+    use RolesOptional;
 }
