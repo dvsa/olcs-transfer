27a0b12a399e179530ab74c7648d4ab2693f1a0b
Ian Lindsay <6673081+ilindsay@users.noreply.github.com>
2024-08-21 09:21:04 +0100

    feat: VOL-5305 add the new operator-tc user to validation for user creation (#86)

--- src/FieldType/Traits/RolesOptional.php
+++ src/FieldType/Traits/RolesOptional.php
@@ -16,7 +16,7 @@ trait RolesOptional
      * @Transfer\Filter("Laminas\Filter\StringTrim")
      * @Transfer\Validator("Laminas\Validator\InArray",
      *     options={
-     *         "haystack": {"internal-limited-read-only", "internal-read-only", "internal-case-worker", "internal-admin", "internal-irhp-admin", "operator-admin", "operator-user", "operator-tm", "partner-admin", "partner-user", "local-authority-admin", "local-authority-user", "system-admin"}
+     *         "haystack": {"internal-limited-read-only", "internal-read-only", "internal-case-worker", "internal-admin", "internal-irhp-admin", "operator-tc", "operator-admin", "operator-user", "operator-tm", "partner-admin", "partner-user", "local-authority-admin", "local-authority-user", "system-admin"}
      *     }
      * )
      * @Transfer\Optional
