78f8c5a4f9a900355f2aae7535de47255670c677
Ian Lindsay <6673081+ilindsay@users.noreply.github.com>
2024-11-01 12:07:09 +0000

    Update src/Command/User/AgreeTerms.php
    Co-authored-by: Andrew Newton <andy.newton@dvsa.gov.uk>

--- src/Command/User/AgreeTerms.php
+++ src/Command/User/AgreeTerms.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Dvsa\Olcs\Transfer\Command\User;
 
-use Dvsa\Olcs\Transfer\FieldType\Traits\Identity;
-use Dvsa\Olcs\Transfer\FieldType\Traits\TranslateToWelshOptional;
 use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 use Dvsa\Olcs\Transfer\Command\AbstractCommand;
 
