88c968808a869c9ee515334d4e9a2dc367f19514
Ahmed Mohamed <ahmed.mohamed2@dvsa.gov.uk>
2025-10-06 14:12:54 +0100

    chore: remove legacy Transxchange publisher code

--- src/Command/Bus/Ebsr/RequestMap.php
+++ src/Command/Bus/Ebsr/RequestMap.php
@@ -17,30 +17,12 @@ class RequestMap extends AbstractCommand
     use Identity;
 
     /**
-     * @var string
      * @Transfer\Validator("Laminas\Validator\InArray", options={"haystack": {"small","large","auto"}})
      */
-    protected $scale;
-
-    /**
-     * @Transfer\Filter("Laminas\Filter\Boolean")
-     * @Transfer\Optional
-     */
-    protected $fromNewEbsr;
-
-    /**
-     * @return string
-     */
-    public function getScale()
+    protected string $scale;
+    
+    public function getScale(): string
     {
         return $this->scale;
-    }
-
-    /**
-     * @return bool
-     */
-    public function getFromNewEbsr()
-    {
-        return $this->fromNewEbsr;
     }
 }
